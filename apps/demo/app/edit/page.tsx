d1a2dc190c614ac9cbad00bfbbcfa27f494e9821
Chris Villa <chrisvilla@me.com>
2023-06-06 11:36:55 +0100

    fix: load initial data if no data

--- apps/demo/app/edit/page.tsx
+++ apps/demo/app/edit/page.tsx
@@ -5,14 +5,18 @@ import config, { initialData } from "../config";
 import { Data } from "core/types/Config";
 
 export default async function Page() {
-  const data: Data = await fetch("http://localhost:3000/api/puck").then((res) =>
+  let data: Data = await fetch("http://localhost:3000/api/puck").then((res) =>
     res.json()
   );
+
+  if (Object.keys(data).length === 0) {
+    data = initialData;
+  }
 
   return (
     <Puck
       config={config}
-      initialData={data || initialData}
+      initialData={data}
       onPublish={async (data) => {
         console.log("Publishing");
 
