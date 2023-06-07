8974b1829aa2ca015044a3555fe005e4e3b4266c
Chris Villa <chrisvilla@me.com>
2023-06-07 09:41:40 +0100

    fix: stop caching editor

--- apps/demo/app/edit/page.tsx
+++ apps/demo/app/edit/page.tsx
@@ -2,9 +2,9 @@ import { Data } from "core/types/Config";
 import Client from "./client";
 
 export default async function Page() {
-  let data: Data = await fetch("http://localhost:3000/api/puck").then((res) =>
-    res.json()
-  );
+  let data: Data = await fetch("http://localhost:3000/api/puck", {
+    next: { revalidate: 0 },
+  }).then((res) => res.json());
 
   return <Client data={data} />;
 }
