6d4a44ab0a3847193bc0595752a97b43c8c926bc
Chris Villa <chrisvilla@me.com>
2023-06-17 00:24:50 +0100

    feat: remove blocks from antd demo

--- apps/demo/app/configs/antd/index.tsx
+++ apps/demo/app/configs/antd/index.tsx
@@ -47,13 +47,6 @@ export const initialData: Record<string, Data> = {
   "/": {
     content: [
       {
-        type: "Video",
-        props: {
-          id: "Video-1686934345327",
-          src: "https://mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*uYT7SZwhJnUAAAAAAAAAAAAADgCCAQ",
-        },
-      },
-      {
         type: "Hero",
         props: {
           id: "Hero-1686932817569",
@@ -63,25 +56,6 @@ export const initialData: Record<string, Data> = {
           ],
           title: "Hello, world",
           description: "This is an example page built with puck and antd.",
-        },
-      },
-      { type: "CardDeck", props: { id: "CardDeck-1686933786390" } },
-      {
-        type: "Carousel",
-        props: {
-          id: "Carousel-1686926848790",
-          slides: [
-            {
-              imageUrl:
-                "https://images.unsplash.com/photo-1686903430777-279ba0f25e7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2750&q=80",
-              alt: "The sun is setting",
-            },
-            {
-              imageUrl:
-                "https://images.unsplash.com/photo-1682685797365-41f45b562c0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
-              alt: "desert sunset",
-            },
-          ],
         },
       },
     ],
