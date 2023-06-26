01722c2af9626e744ca3975fc9f74ebff3d801c6
Chris Villa <chrisvilla@me.com>
2023-06-26 15:59:55 +0100

    feat: remove hardcoded API token from Strapi adaptor

--- packages/adaptors/adaptors/strapi.ts
+++ packages/adaptors/adaptors/strapi.ts
@@ -1,6 +1,7 @@
 import { Adaptor } from "core/types/Config";
 
-type AdaptorParams = { resource: string };
+type AdaptorParams = { apiToken: string; resource: string };
+
 export const strapiAdaptor: Adaptor<AdaptorParams> = {
   name: "Strapi.js",
   fetchList: async (adaptorParams) => {
@@ -12,8 +13,7 @@ export const strapiAdaptor: Adaptor<AdaptorParams> = {
       `http://localhost:1337/api/${adaptorParams.resource}`,
       {
         headers: {
-          Authorization:
-            "bearer 76ef70c739ec2fe718bfff35fa64384f1ada1d11ca83e3deb432e23304dfddbbe295f785d7c6fe7985a0e7f138633edf8c8a4eb1c4208cd5734f3220c19c0736567a5d3cacc20bba37a1e7fbc9e83ce0e808ce8ad1055f10d3f1eec4a42f378521df8dd12f839930382d18bda55def0615d3ea2d5fdef5196406695c13c44102",
+          Authorization: `bearer ${adaptorParams.apiToken}`,
         },
       }
     );
--- recipes/next/puck.config.tsx
+++ recipes/next/puck.config.tsx
@@ -84,7 +84,12 @@ export const config: Config<Props> = {
         _data: {
           type: "external",
           adaptor: strapiAdaptor,
-          adaptorParams: { resource: "movies" },
+          adaptorParams: {
+            // Not a real API token
+            apiToken:
+              "76ef70c739ec2fe718bfff35fa64384f1ada1d11ca83e3deb432e23304dfddbbe295f785d7c6fe7985a0e7f138633edf8c8a4eb1c4208cd5734f3220c19c0736567a5d3cacc20bba37a1e7fbc9e83ce0e808ce8ad1055f10d3f1eec4a42f378521df8dd12f839930382d18bda55def0615d3ea2d5fdef5196406695c13c44102",
+            resource: "movies",
+          },
         },
         // TODO assume this input type as default
         title: { type: "text" },
