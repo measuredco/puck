9f7abc4f942a8378a81bc6cda4b479592e399c23
Chris Villa <chrisvilla@me.com>
2023-06-27 16:55:20 +0100

    fix: add missing lib

---
+++ packages/core/lib/scroll-into-view.ts
@@ -0,0 +1,11 @@
+export const scrollIntoView = (el: HTMLElement) => {
+  const oldStyle = { ...el.style };
+
+  el.style.scrollMargin = "256px";
+
+  if (el) {
+    el?.scrollIntoView({ behavior: "smooth" });
+
+    el.style.scrollMargin = oldStyle.scrollMargin;
+  }
+};
