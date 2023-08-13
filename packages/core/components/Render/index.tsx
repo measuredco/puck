d7e31901626734ce43cd9161971d9811b6d5c483
Chris Villa <chrisvilla@me.com>
2023-08-13 11:05:57 +0100

    feat: gracefully fallback if component definition doesn't exist

--- packages/core/components/Render/index.tsx
+++ packages/core/components/Render/index.tsx
@@ -4,9 +4,13 @@ import { Config, Data } from "../../types/Config";
 
 export function Render({ config, data }: { config: Config; data: Data }) {
   const children = data.content.map((item) => {
-    const Component = config.components[item.type].render;
+    const Component = config.components[item.type]?.render;
 
-    return <Component key={item.props.id} {...item.props} />;
+    if (Component) {
+      return <Component key={item.props.id} {...item.props} />;
+    }
+
+    return null;
   });
 
   if (config.root) {
