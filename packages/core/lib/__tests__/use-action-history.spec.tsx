57a55c3d67ba9a1db864c3af69b0de01b9cdccec
Yuddomack <darr880406@gmail.com>
2023-09-12 14:15:31 +0900

    feat: init canForward, canRewind

--- packages/core/lib/__tests__/use-action-history.spec.tsx
+++ packages/core/lib/__tests__/use-action-history.spec.tsx
@@ -1,7 +1,30 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, type RenderHookResult } from "@testing-library/react";
 import { useActionHistory } from "../use-action-history";
 
-test("test", () => {
-  const { result } = renderHook(() => useActionHistory());
-  expect(result.current.histories).toEqual([]);
+describe("given nothing", () => {
+  let renderedHook: RenderHookResult<ReturnType<typeof useActionHistory>, any>;
+
+  beforeEach(() => {
+    renderedHook = renderHook(() => useActionHistory());
+  });
+
+  test("canForward should be false when there are no records", () => {
+    const {
+      result: {
+        current: { canForward },
+      },
+    } = renderedHook;
+
+    expect(canForward).toBe(false);
+  });
+
+  test("canRewind should be false when there are no records", () => {
+    const {
+      result: {
+        current: { canRewind },
+      },
+    } = renderedHook;
+
+    expect(canRewind).toBe(false);
+  });
 });
--- packages/core/lib/use-action-history.ts
+++ packages/core/lib/use-action-history.ts
@@ -3,5 +3,8 @@ import { useState } from "react";
 export function useActionHistory() {
   const [histories, setHistories] = useState([]);
 
-  return { histories, setHistories };
+  const canForward = false;
+  const canRewind = false;
+
+  return { histories, setHistories, canForward, canRewind };
 }
