3cdcb7be7615a2a6e2c6e22d60570a823a1e1b90
Chris Villa <chrisvilla@me.com>
2023-09-15 16:15:56 +0100

    docs: showcase DropZone API in demo application

---
+++ apps/demo/app/configs/custom/blocks/Columns/index.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { ComponentConfig } from "@measured/puck/types/Config";
+import styles from "./styles.module.css";
+import { getClassNameFactory } from "@measured/puck/lib";
+import { DropZone } from "@measured/puck/components/DropZone";
+import { Section } from "../../components/Section";
+
+const getClassName = getClassNameFactory("Columns", styles);
+
+export type ColumnsProps = {
+  distribution: "auto" | "manual";
+  columns: {
+    span?: number;
+  }[];
+};
+
+export const Columns: ComponentConfig<ColumnsProps> = {
+  fields: {
+    distribution: {
+      type: "radio",
+      options: [
+        {
+          value: "auto",
+          label: "Auto",
+        },
+        {
+          value: "manual",
+          label: "Manual",
+        },
+      ],
+    },
+    columns: {
+      type: "array",
+      getItemSummary: (col, id) =>
+        `Column ${id + 1}, span ${
+          col.span ? Math.max(Math.min(col.span, 12), 1) : "auto"
+        }`,
+      arrayFields: {
+        span: {
+          label: "Span (1-12)",
+          type: "number",
+        },
+      },
+    },
+  },
+  defaultProps: {
+    distribution: "auto",
+    columns: [{}, {}],
+  },
+  render: ({ columns, distribution }) => {
+    return (
+      <Section>
+        <div
+          className={getClassName()}
+          style={{
+            gridTemplateColumns:
+              distribution === "manual"
+                ? "repeat(12, 1fr)"
+                : `repeat(${columns.length}, 1fr)`,
+          }}
+        >
+          {columns.map(({ span }, idx) => (
+            <div
+              key={idx}
+              style={{
+                gridColumn:
+                  span && distribution === "manual"
+                    ? `span ${Math.max(Math.min(span, 12), 1)}`
+                    : "",
+              }}
+            >
+              <DropZone dropzone={`column-${idx}`} />
+            </div>
+          ))}
+        </div>
+      </Section>
+    );
+  },
+};
---
+++ apps/demo/app/configs/custom/blocks/Flex/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { ComponentConfig } from "@measured/puck/types/Config";
+import styles from "./styles.module.css";
+import { getClassNameFactory } from "@measured/puck/lib";
+import { DropZone } from "@measured/puck/components/DropZone";
+import { Section } from "../../components/Section";
+
+const getClassName = getClassNameFactory("Flex", styles);
+
+export type FlexProps = {
+  items: { minItemWidth?: number }[];
+  minItemWidth: number;
+};
+
+export const Flex: ComponentConfig<FlexProps> = {
+  fields: {
+    items: {
+      type: "array",
+      arrayFields: {
+        minItemWidth: {
+          label: "Minimum Item Width",
+          type: "number",
+        },
+      },
+      getItemSummary: (_, id) => `Item ${id + 1}`,
+    },
+    minItemWidth: {
+      label: "Minimum Item Width",
+      type: "number",
+    },
+  },
+  defaultProps: {
+    items: [{}, {}],
+    minItemWidth: 356,
+  },
+  render: ({ items, minItemWidth }) => {
+    return (
+      <Section>
+        <div className={getClassName()}>
+          {items.map((item, idx) => (
+            <div
+              key={idx}
+              className={getClassName("item")}
+              style={{ minWidth: item.minItemWidth || minItemWidth }}
+            >
+              <DropZone dropzone={`item-${idx}`} />
+            </div>
+          ))}
+        </div>
+      </Section>
+    );
+  },
+};
