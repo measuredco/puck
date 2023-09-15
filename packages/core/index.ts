5053a8430de1f4bfb6fb7a4b1f194a1474ed3ae3
Chris Villa <chrisvilla@me.com>
2023-09-15 16:29:43 +0100

    feat: introduce DropZone API for nesting components and advanced layouts

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,6 +2,7 @@ import "./styles/global.css";
 
 export * from "./types/Config";
 export * from "./components/Button";
+export * from "./components/DropZone";
 export * from "./components/IconButton";
 export * from "./components/Puck";
 export * from "./components/Render";
