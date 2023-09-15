a9a273bc4913eaea007b2c1dcbe3b6d1c605677b
Chris Villa <chrisvilla@me.com>
2023-09-15 16:23:36 +0100

    test: fix linting issues in use-modifier-held helper
    Helper is unused following the DropZone API implementation, but may be reintroduced at a later date.

--- packages/core/lib/use-modifier-held.ts
+++ packages/core/lib/use-modifier-held.ts
@@ -3,26 +3,26 @@ import { useEffect, useState } from "react";
 export const useModifierHeld = (modifier: "Shift" | "Alt" | "Control") => {
   const [modifierHeld, setModifierHeld] = useState(false);
 
-  function downHandler({ key }) {
-    if (key === modifier) {
-      setModifierHeld(true);
-    }
-  }
-
-  function upHandler({ key }) {
-    if (key === modifier) {
-      setModifierHeld(false);
-    }
-  }
-
   useEffect(() => {
+    function downHandler({ key }) {
+      if (key === modifier) {
+        setModifierHeld(true);
+      }
+    }
+
+    function upHandler({ key }) {
+      if (key === modifier) {
+        setModifierHeld(false);
+      }
+    }
+
     window.addEventListener("keydown", downHandler);
     window.addEventListener("keyup", upHandler);
     return () => {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
-  }, []);
+  }, [modifier]);
 
   return modifierHeld;
 };
