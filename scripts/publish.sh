b089a7d43e8abba5bec22b9da272c36aeb6f2cf2
Chris Villa <chrisvilla@me.com>
2023-09-20 15:51:48 +0100

    build: remove dry-run from new npm publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,11 @@
-cd packages/core && npm publish --dry-run --access public --tag $1
+cd packages/core && npm publish --access public --tag $1
 cd ../../
 
-cd packages/adaptor-strapi && npm publish --dry-run --access public --tag $1
+cd packages/adaptor-strapi && npm publish --access public --tag $1
 cd ../../
 
-cd packages/plugin-heading-analyzer && npm publish --dry-run --access public --tag $1
+cd packages/plugin-heading-analyzer && npm publish --access public --tag $1
 cd ../../
 
-cd packages/create-puck-app && npm publish --dry-run --access public --tag $1
+cd packages/create-puck-app && npm publish --access public --tag $1
 cd ../../
