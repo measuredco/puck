dd85615643583e9c8f1a6fc21f0fae36a0747d83
Chris Villa <chrisvilla@me.com>
2023-09-21 15:13:22 +0100

    fix: add missing id type to render props

--- packages/core/components/Render/index.tsx
+++ packages/core/components/Render/index.tsx
@@ -8,7 +8,7 @@ export function Render({ config, data }: { config: Config; data: Data }) {
   if (config.root) {
     return (
       <DropZoneProvider value={{ data, config, mode: "render" }}>
-        <config.root.render {...data.root} editMode={false}>
+        <config.root.render {...data.root} editMode={false} id={"puck-root"}>
           <DropZone zone={rootDroppableId} />
         </config.root.render>
       </DropZoneProvider>
