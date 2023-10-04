c9757f714c866ac42938e6b190737d6fee46e591
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2023-10-04 16:37:37 +0100

    Adding mdx to prettier and running it on the files

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,7 +1,7 @@
 const theme = {
   logo: <strong>Puck</strong>,
   project: {
-    link: 'https://github.com/measuredco/puck'
+    link: "https://github.com/measuredco/puck",
   },
   footer: {
     text: (
@@ -10,27 +10,36 @@ const theme = {
           MIT © {new Date().getFullYear()} Measured Co.
         </p>
       </div>
-    )
+    ),
   },
   chat: {
-    link: 'https://discord.gg/D9e4E3MQVZ',
+    link: "https://discord.gg/D9e4E3MQVZ",
     icon: (
-      <img alt="" src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg" height={32} width={32}/>
-    )
+      <img
+        alt=""
+        src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg"
+        height={32}
+        width={32}
+      />
+    ),
   },
   toc: {
-    backToTop: true
+    backToTop: true,
   },
   banner: {
-    key: '0.8.0-release',
+    key: "0.8.0-release",
     text: (
-      <a href="https://github.com/measuredco/puck/releases/tag/v0.8.0" target="_blank">
-        🎉 Puck 0.8.0 is released which includes nested DropZones, new outline UI and much more. →
+      <a
+        href="https://github.com/measuredco/puck/releases/tag/v0.8.0"
+        target="_blank"
+      >
+        🎉 Puck 0.8.0 is released which includes nested DropZones, new outline
+        UI and much more. →
       </a>
-    )
+    ),
   },
-  docsRepositoryBase: 'https://github.com/measuredco/puck/tree/main/app/docs',
-  primarySaturation: 0
-}
+  docsRepositoryBase: "https://github.com/measuredco/puck/tree/main/app/docs",
+  primarySaturation: 0,
+};
 
-export default theme
+export default theme;
