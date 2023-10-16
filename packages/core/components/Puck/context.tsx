dd3804547d63f889fc66de304c1b6e12a8f98ed1
Chris Villa <chrisvilla@me.com>
2023-10-16 13:50:12 +0100

    refactor: add global app context to expose AppData

---
+++ packages/core/components/Puck/context.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext } from "react";
+import { AppData } from "../../types/Config";
+import { PuckAction } from "../../reducer";
+
+export const defaultAppData: AppData = {
+  data: { content: [], root: { title: "" } },
+  state: {
+    leftSideBarVisible: true,
+    arrayFieldConfig: {},
+  },
+};
+
+type AppContext = {
+  appData: AppData;
+  dispatch: (action: PuckAction) => void;
+};
+
+export const appContext = createContext<AppContext>({
+  appData: defaultAppData,
+  dispatch: () => null,
+});
+
+export const AppProvider = appContext.Provider;
+
+export const useAppContext = () => {
+  const mainContext = useContext(appContext);
+
+  return {
+    ...mainContext,
+    // Helper
+    setState: (state: Partial<AppState>, recordHistory?: boolean) => {
+      return mainContext.dispatch({
+        type: "setState",
+        state,
+        recordHistory,
+      });
+    },
+  };
+};
