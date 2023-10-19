360383c3a6bb3ef8f6f46998325ca0ed17518704
Chris Villa <chrisvilla@me.com>
2023-10-19 11:42:23 +0100

    docs: add code docs to next recipe

--- recipes/next/app/puck/[...puckPath]/page.tsx
+++ recipes/next/app/puck/[...puckPath]/page.tsx
@@ -1,3 +1,16 @@
+/**
+ * This file implements a *magic* catch-all route that renders the Puck editor.
+ *
+ * This route exposes /puck/[...puckPath], but is disabled by middleware.ts. The middleware
+ * then rewrites all URL requests ending in `/edit` to this route, allowing you to visit any
+ * page in your application and add /edit to the end to spin up a Puck editor.
+ *
+ * This approach enables public pages to be statically rendered whilst the /puck route can
+ * remain dynamic.
+ *
+ * NB this route is public, and you will need to add authentication
+ */
+
 import { Client } from "./client";
 import { Metadata } from "next";
 import { getPage } from "../../../lib/get-page";
