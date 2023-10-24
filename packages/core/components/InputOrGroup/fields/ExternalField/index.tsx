a3a018bb1876fd4b831676e8ff848052ec7ba527
Chris Villa <chrisvilla@me.com>
2023-10-24 23:30:17 +0100

    feat: add icon to adaptor inputs

--- packages/core/components/InputOrGroup/fields/ExternalField/index.tsx
+++ packages/core/components/InputOrGroup/fields/ExternalField/index.tsx
@@ -2,13 +2,13 @@ import getClassNameFactory from "../../../../lib/get-class-name-factory";
 import styles from "../../styles.module.css";
 import type { InputProps } from "../..";
 import { ExternalInput } from "../../../ExternalInput";
+import { Link } from "react-feather";
 
 const getClassName = getClassNameFactory("Input", styles);
 
 export const ExternalField = ({
   field,
   onChange,
-  readOnly,
   value,
   name,
   label,
@@ -18,9 +18,13 @@ export const ExternalField = ({
   }
 
   return (
-    <div className={getClassName("")}>
+    <div className={getClassName()}>
       <div className={getClassName("label")}>
-        {name === "_data" ? "External content" : label || name}
+        <div className={getClassName("labelIcon")}>
+          <Link size={16} />
+        </div>
+
+        {name === "_data" ? label || "External content" : label || name}
       </div>
       <ExternalInput field={field} onChange={onChange} value={value} />
     </div>
