29da24eeb4d311a98f238cf124c5ffcf1f57f142
Chris Villa <chrisvilla@me.com>
2023-11-08 08:46:38 +0000

    docs: add basic layout to docs site homepage

---
+++ apps/docs/components/Home/index.tsx
@@ -0,0 +1,63 @@
+export const Home = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        paddingTop: 196,
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <div style={{ maxWidth: 1128 }}>
+        <h1 style={{ visibility: "hidden" }}>Puck</h1>
+
+        <h2 style={{ fontSize: 64, fontWeight: 700, lineHeight: 1.2 }}>
+          The self-hosted visual editor for React
+        </h2>
+      </div>
+      <div style={{ maxWidth: 896 }}>
+        <div style={{ paddingTop: 24 }} />
+        <p style={{ fontSize: 18, lineHeight: 1.5, opacity: 0.7 }}>
+          Puck empowers developers to build amazing visual editing experiences
+          into their own React application, powering the next generation of
+          content tools, no-code builders and WYSIWYG editors.
+        </p>
+        <div style={{ paddingTop: 32 }} />
+        <div style={{ display: "flex", justifyContent: "center", gap: 16 }}>
+          <a
+            style={{
+              padding: "12px 16px",
+              backgroundColor: "#2f539e",
+              color: "white",
+              display: "block",
+              borderRadius: 8,
+              fontWeight: 500,
+            }}
+            href="/docs/installation"
+          >
+            Get started
+          </a>
+          <a
+            style={{
+              padding: "12px 16px",
+              border: "1px solid currentColor",
+              display: "block",
+              borderRadius: 8,
+              fontWeight: 500,
+            }}
+            href="https://puck-demo-measured.vercel.app/edit"
+          >
+            View demo
+          </a>
+        </div>{" "}
+        <div style={{ paddingTop: 32 }} />
+        <pre style={{ padding: 0, margin: 0 }}>
+          <span style={{ userSelect: "none" }}>~ </span>npm i @measured/puck
+          --save
+        </pre>
+      </div>
+      <div style={{ paddingTop: 196 }} />
+    </div>
+  );
+};
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,5 +1,30 @@
-const theme = {
-  logo: <strong>Puck</strong>,
+import { useRouter } from "next/router";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const theme: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Puck",
+      };
+    }
+  },
+  logo: (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1300 326"
+        width="1300"
+        height="326"
+        style={{ width: 128, marginBottom: -5, height: 32 }}
+        fill="currentColor"
+      >
+        <path d="M368.9 5.9H455c48.1 0 88.1 15.4 88.1 70.4 0 54.4-37 71.1-85.8 71.1H420v90.4h-51.1V5.9zm51.1 98.2h34c18 0 36-6.2 36-27.8 0-23.9-24.2-27.2-43.9-27.2H420v55zM786 148.3c0 54.7-33.4 95.3-97.6 95.3-64.5 0-97.9-40.6-97.9-95.3V5.9h51.1v140.5c0 28.5 19.6 50.1 46.8 50.1 26.8 0 46.5-21.6 46.5-50.1V5.9H786v142.4zM997.1 66.1c-10.1-12.1-24.9-19-43.9-19-38.6 0-67.1 31.4-67.1 74.7s28.5 74.7 65.5 74.7c20.6 0 37.3-9.2 47.8-24.9l42.6 31.8c-19.3 27.5-52.1 40.3-83.8 40.3-72.4 0-125.1-47.5-125.1-121.8C833.1 47.5 885.8 0 958.2 0c25.9 0 58.6 8.8 78.3 34.1l-39.4 32zM1083.2 5.9h51.1v96.3l90-96.3h66.8L1188 113.6l112 124.1h-71.4l-94.3-110v110h-51.1V5.9zM149.3 237.7H82.5v-24.4h66.9v24.4zm82.5-82.5h-24.4V88.4h24.4v66.8zm-207.4 0H0V88.4h24.4v66.8zM149.3 30.3H82.5V5.9h66.9v24.4zM45.6 237.7H0v-45.6h24.4v21.2h21.2v24.4zM231.8 51.5h-24.4V30.3h-21.2V5.9h45.6v45.6zm-207.4 0H0V5.9h45.6v24.4H24.4v21.2zM164.8 170.7l27.5 155.2L320 198.2l-155.2-27.5z" />
+      </svg>
+    </div>
+  ),
   project: {
     link: "https://github.com/measuredco/puck",
   },
@@ -15,26 +40,31 @@ const theme = {
   chat: {
     link: "https://discord.gg/D9e4E3MQVZ",
     icon: (
-      <img
-        alt=""
-        src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg"
-        height={32}
-        width={32}
-      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        preserveAspectRatio="xMidYMid"
+        viewBox="0 -28.5 256 256"
+        fill="currentColor"
+      >
+        <path d="M216.856 16.597A208.502 208.502 0 0 0 164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046-19.692-2.961-39.203-2.961-58.533 0-1.832-4.4-4.55-9.933-6.846-14.046a207.809 207.809 0 0 0-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161.094 161.094 0 0 0 79.735 175.3a136.413 136.413 0 0 1-21.846-10.632 108.636 108.636 0 0 0 5.356-4.237c42.122 19.702 87.89 19.702 129.51 0a131.66 131.66 0 0 0 5.355 4.237 136.07 136.07 0 0 1-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848 21.142-6.58 42.646-16.637 64.815-33.213 5.316-56.288-9.08-105.09-38.056-148.36ZM85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2c12.867 0 23.236 11.804 23.015 26.2.02 14.375-10.148 26.18-23.015 26.18Zm85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2 0 14.375-10.148 26.18-23.015 26.18Z" />
+      </svg>
     ),
   },
   toc: {
     backToTop: true,
   },
   banner: {
-    key: "0.8.0-release",
+    dismissible: true,
+    key: "open-source",
     text: (
       <a
-        href="https://github.com/measuredco/puck/releases/tag/v0.8.0"
+        href="https://github.com/measuredco/puck/releases/tag/v0.11.0"
         target="_blank"
       >
-        🎉 Puck 0.8.0 is released which includes nested DropZones, new outline
-        UI and much more. →
+        <b>🎉 Puck 0.11.0 out now</b>: includes the new categories API for
+        grouping components, and new ways to load external data →
       </a>
     ),
   },
