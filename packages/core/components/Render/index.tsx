ffec7ea2ee5a6457f60876a79c005e49c1153912
Jon Vivian <jonv@datacom.co.nz>
2023-11-08 22:15:22 +1300

    revert use ServerRender for existing Render component

--- packages/core/components/Render/index.tsx
+++ packages/core/components/Render/index.tsx
@@ -1,15 +1,36 @@
 "use client";
 
+import { rootDroppableId } from "../../lib/root-droppable-id";
 import { Config, Data } from "../../types/Config";
-import { DropZoneProvider } from "../DropZone";
-import { Render as ServerRender } from "../ServerRender";
+import { DropZone, DropZoneProvider } from "../DropZone";
 
 export function Render({ config, data }: { config: Config; data: Data }) {
+  // DEPRECATED
+  const rootProps = data.root.props || data.root;
+
+  const title = rootProps.title || "";
+
+  if (config.root?.render) {
+    return (
+      <DropZoneProvider value={{ data, config, mode: "render" }}>
+        <config.root.render
+          {...rootProps}
+          puck={{
+            renderDropZone: DropZone,
+          }}
+          title={title}
+          editMode={false}
+          id={"puck-root"}
+        >
+          <DropZone zone={rootDroppableId} />
+        </config.root.render>
+      </DropZoneProvider>
+    );
+  }
+
   return (
-    // DEPRECATED
-    // Can be removed once exporting DropZone is removed
     <DropZoneProvider value={{ data, config, mode: "render" }}>
-      <ServerRender config={config} data={data} />
+      <DropZone zone={rootDroppableId} />
     </DropZoneProvider>
   );
 }
