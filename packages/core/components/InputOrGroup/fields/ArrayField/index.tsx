db0c6b92e9f735d5290cd93b6fc23375be845d3a
Chris Villa <chrisvilla@me.com>
2023-11-15 12:18:43 +0000

    fix: don't reset array item labels when changing order
    When the user is relying on the default label (i.e. Item 1) for items in an array field, the index would reset whenever the user dragged the items between positions. That was because the label was based on the index.
    
    This fix stores the original index for the item in the arrayState, which persists between drags.
    
    This also introduces some refactors that may improve the general robustness of array fields.

--- packages/core/components/InputOrGroup/fields/ArrayField/index.tsx
+++ packages/core/components/InputOrGroup/fields/ArrayField/index.tsx
@@ -8,7 +8,7 @@ import { Droppable } from "@hello-pangea/dnd";
 import { DragDropContext } from "@hello-pangea/dnd";
 import { Draggable } from "../../../Draggable";
 import { generateId } from "../../../../lib/generate-id";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { DragIcon } from "../../../DragIcon";
 import { ArrayState, ItemWithId } from "../../../../types/Config";
 import { useAppContext } from "../../../Puck/context";
@@ -29,35 +29,54 @@ export const ArrayField = ({
 
   const { state, setUi } = useAppContext();
 
-  const arrayState: ArrayState = state.ui.arrayState[arrayFieldId] || {
-    items: Array.from(value || []).map<ItemWithId>((v) => ({
-      _arrayId: generateId("ArrayItem"),
-      data: v,
-    })),
-    openId: "",
-  };
+  const arrayStateRef = useRef<ArrayState>(
+    state.ui.arrayState[arrayFieldId] || {
+      items: [],
+      openId: "",
+    }
+  );
+
+  const arrayState = arrayStateRef.current;
 
   const setArrayState = useCallback(
-    (newArrayState: Partial<ArrayState>, recordHistory: boolean = false) => {
+    (
+      partialArrayState: Partial<ArrayState>,
+      recordHistory: boolean = false
+    ) => {
       setUi(
         {
           arrayState: {
             ...state.ui.arrayState,
-            [arrayFieldId]: { ...arrayState, ...newArrayState },
+            [arrayFieldId]: { ...arrayState, ...partialArrayState },
           },
         },
         recordHistory
       );
+
+      arrayStateRef.current = { ...arrayState, ...partialArrayState };
     },
     [arrayState]
   );
 
   // Create a mirror of value with IDs added for drag and drop
   useEffect(() => {
-    const newItems = Array.from(value || []).map((item, idx) => ({
-      _arrayId: arrayState.items[idx]?._arrayId || generateId("ArrayItem"),
-      data: item,
-    }));
+    let highestIndex = arrayState.items.reduce(
+      (acc, item) => (item._originalIndex > acc ? item._originalIndex : acc),
+      -1
+    );
+
+    const newItems = Array.from(value || []).map((item, idx) => {
+      const arrayStateItem = arrayState.items[idx];
+
+      return {
+        _originalIndex:
+          typeof arrayStateItem?._originalIndex !== "undefined"
+            ? arrayStateItem._originalIndex
+            : ++highestIndex,
+        _arrayId: arrayState.items[idx]?._arrayId || generateId("ArrayItem"),
+        data: item,
+      };
+    });
 
     // We don't need to record history during this useEffect, as the history has already been set by onDragEnd
     setArrayState({ items: newItems });
@@ -99,120 +118,124 @@ export const ArrayField = ({
                   hasItems: Array.isArray(value) && value.length > 0,
                 })}
               >
-                {Array.isArray(value) && value.length > 0
-                  ? arrayState.items.map(({ _arrayId, data }, i) => (
-                      <Draggable
-                        id={_arrayId}
-                        index={i}
-                        key={_arrayId}
-                        className={(_, snapshot) =>
-                          getClassNameItem({
-                            isExpanded: arrayState.openId === _arrayId,
-                            isDragging: snapshot.isDragging,
-                            readOnly,
-                          })
-                        }
-                        isDragDisabled={readOnly}
-                      >
-                        {() => (
-                          <>
-                            <div
-                              onClick={() => {
-                                if (arrayState.openId === _arrayId) {
-                                  setArrayState({
-                                    openId: "",
-                                  });
-                                } else {
-                                  setArrayState({
-                                    openId: _arrayId,
-                                  });
-                                }
-                              }}
-                              className={getClassNameItem("summary")}
-                            >
-                              {field.getItemSummary
-                                ? field.getItemSummary(data, i)
-                                : `Item #${i}`}
-                              <div className={getClassNameItem("rhs")}>
-                                {!readOnly && (
-                                  <div className={getClassNameItem("actions")}>
-                                    <div className={getClassNameItem("action")}>
-                                      <IconButton
-                                        onClick={() => {
-                                          const existingValue = [
-                                            ...(value || []),
-                                          ];
-                                          const existingItems = [
-                                            ...(arrayState.items || []),
-                                          ];
+                {arrayState.items.map(({ data }, i) => {
+                  const { _arrayId, _originalIndex = i } =
+                    arrayState.items[i] || {};
 
-                                          existingValue.splice(i, 1);
-                                          existingItems.splice(i, 1);
+                  return (
+                    <Draggable
+                      id={_arrayId}
+                      index={i}
+                      key={_arrayId}
+                      className={(_, snapshot) =>
+                        getClassNameItem({
+                          isExpanded: arrayState.openId === _arrayId,
+                          isDragging: snapshot.isDragging,
+                          readOnly,
+                        })
+                      }
+                      isDragDisabled={readOnly}
+                    >
+                      {() => (
+                        <>
+                          <div
+                            onClick={() => {
+                              if (arrayState.openId === _arrayId) {
+                                setArrayState({
+                                  openId: "",
+                                });
+                              } else {
+                                setArrayState({
+                                  openId: _arrayId,
+                                });
+                              }
+                            }}
+                            className={getClassNameItem("summary")}
+                          >
+                            {field.getItemSummary
+                              ? field.getItemSummary(data, i)
+                              : `Item #${_originalIndex}`}
+                            <div className={getClassNameItem("rhs")}>
+                              {!readOnly && (
+                                <div className={getClassNameItem("actions")}>
+                                  <div className={getClassNameItem("action")}>
+                                    <IconButton
+                                      onClick={(e) => {
+                                        e.stopPropagation();
 
-                                          setArrayState(
-                                            { items: existingItems },
-                                            true
-                                          );
-                                          onChange(existingValue);
-                                        }}
-                                        title="Delete"
-                                      >
-                                        <Trash size={16} />
-                                      </IconButton>
-                                    </div>
+                                        const existingValue = [
+                                          ...(value || []),
+                                        ];
+
+                                        const existingItems = [
+                                          ...(arrayState.items || []),
+                                        ];
+
+                                        existingValue.splice(i, 1);
+                                        existingItems.splice(i, 1);
+
+                                        setArrayState(
+                                          { items: existingItems },
+                                          true
+                                        );
+
+                                        onChange(existingValue);
+                                      }}
+                                      title="Delete"
+                                    >
+                                      <Trash size={16} />
+                                    </IconButton>
                                   </div>
-                                )}
-                                <div>
-                                  <DragIcon />
                                 </div>
+                              )}
+                              <div>
+                                <DragIcon />
                               </div>
                             </div>
-                            <div className={getClassNameItem("body")}>
-                              <fieldset
-                                className={getClassNameItem("fieldset")}
-                              >
-                                {Object.keys(field.arrayFields!).map(
-                                  (fieldName) => {
-                                    const subField =
-                                      field.arrayFields![fieldName];
+                          </div>
+                          <div className={getClassNameItem("body")}>
+                            <fieldset className={getClassNameItem("fieldset")}>
+                              {Object.keys(field.arrayFields!).map(
+                                (fieldName) => {
+                                  const subField =
+                                    field.arrayFields![fieldName];
 
-                                    const subFieldName = `${name}[${i}].${fieldName}`;
-                                    const wildcardFieldName = `${name}[*].${fieldName}`;
+                                  const subFieldName = `${name}[${i}].${fieldName}`;
+                                  const wildcardFieldName = `${name}[*].${fieldName}`;
 
-                                    return (
-                                      <InputOrGroup
-                                        key={subFieldName}
-                                        name={subFieldName}
-                                        label={subField.label || fieldName}
-                                        readOnly={
-                                          typeof readOnlyFields[
-                                            subFieldName
-                                          ] !== "undefined"
-                                            ? readOnlyFields[subFieldName]
-                                            : readOnlyFields[wildcardFieldName]
-                                        }
-                                        readOnlyFields={readOnlyFields}
-                                        field={subField}
-                                        value={data[fieldName]}
-                                        onChange={(val) =>
-                                          onChange(
-                                            replace(value, i, {
-                                              ...data,
-                                              [fieldName]: val,
-                                            })
-                                          )
-                                        }
-                                      />
-                                    );
-                                  }
-                                )}
-                              </fieldset>
-                            </div>
-                          </>
-                        )}
-                      </Draggable>
-                    ))
-                  : null}
+                                  return (
+                                    <InputOrGroup
+                                      key={subFieldName}
+                                      name={subFieldName}
+                                      label={subField.label || fieldName}
+                                      readOnly={
+                                        typeof readOnlyFields[subFieldName] !==
+                                        "undefined"
+                                          ? readOnlyFields[subFieldName]
+                                          : readOnlyFields[wildcardFieldName]
+                                      }
+                                      readOnlyFields={readOnlyFields}
+                                      field={subField}
+                                      value={data[fieldName]}
+                                      onChange={(val) =>
+                                        onChange(
+                                          replace(value, i, {
+                                            ...data,
+                                            [fieldName]: val,
+                                          })
+                                        )
+                                      }
+                                    />
+                                  );
+                                }
+                              )}
+                            </fieldset>
+                          </div>
+                        </>
+                      )}
+                    </Draggable>
+                  );
+                })}
 
                 {provided.placeholder}
 
