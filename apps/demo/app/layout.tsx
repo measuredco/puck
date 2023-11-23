62ff4327285fd36b7f0161f83974de56c359fafb
Chris Villa <chrisvilla@me.com>
2023-11-23 09:47:10 +0000

    chore: add privacy-first analytics to demo

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -15,6 +15,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN && (
+          <script
+            defer
+            data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN}
+            src="https://plausible.io/js/plausible.js"
+          ></script>
+        )}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
