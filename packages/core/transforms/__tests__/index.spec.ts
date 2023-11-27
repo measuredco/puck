7a234175641da68ba5d480be6cda2e245100bbbf
Chris Villa <chrisvilla@me.com>
2023-11-27 18:51:17 +0000

    test: test transformProp helper

--- packages/core/transforms/__tests__/index.spec.ts
+++ packages/core/transforms/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { migrate } from "..";
+import { migrate, transformProps } from "..";
 
 jest.spyOn(console, "warn").mockImplementation(() => {});
 
@@ -8,5 +8,80 @@ describe("migrate method", () => {
       content: [],
       root: { props: { title: "Hello, world" } },
     });
+  });
+});
+
+describe("transformProps method", () => {
+  it("should migrate props for the root", () => {
+    expect(
+      transformProps(
+        { content: [], root: { props: { title: "Hello, world" } } },
+        {
+          root: (props) => ({
+            updatedTitle: props.title,
+          }),
+        }
+      )
+    ).toEqual({
+      content: [],
+      root: { props: { updatedTitle: "Hello, world" } },
+      zones: {},
+    });
+  });
+
+  it("should migrate props for a specified component", () => {
+    expect(
+      transformProps(
+        {
+          content: [
+            {
+              type: "HeadingBlock",
+              props: { title: "Hello, world", id: "123" },
+            },
+          ],
+
+          root: { props: { title: "Hello, world" } },
+          zones: {
+            MyZone: [
+              {
+                type: "HeadingBlock",
+                props: { title: "Hello, other world", id: "456" },
+              },
+            ],
+          },
+        },
+        {
+          HeadingBlock: (props) => ({
+            heading: props.title,
+          }),
+        }
+      )
+    ).toMatchInlineSnapshot(`
+      {
+        "content": [
+          {
+            "props": {
+              "heading": "Hello, world",
+            },
+            "type": "HeadingBlock",
+          },
+        ],
+        "root": {
+          "props": {
+            "title": "Hello, world",
+          },
+        },
+        "zones": {
+          "MyZone": [
+            {
+              "props": {
+                "heading": "Hello, other world",
+              },
+              "type": "HeadingBlock",
+            },
+          ],
+        },
+      }
+    `);
   });
 });
