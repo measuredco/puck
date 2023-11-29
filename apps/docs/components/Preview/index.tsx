b52745164d1f00275e3cb77e4124efbe2f43f093
Chris Villa <chrisvilla@me.com>
2023-11-29 17:32:36 +0000

    refactor: integrate composable Puck into docs, instead of reimplementing

--- apps/docs/components/Preview/index.tsx
+++ apps/docs/components/Preview/index.tsx
@@ -1,91 +1,96 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 export { InputOrGroup } from "@/core/components/InputOrGroup";
 
-import { ReactNode, useReducer, useState } from "react";
+import { ReactNode } from "react";
 import "@/core/styles.css";
-import { AppProvider, defaultAppState } from "@/core/components/Puck/context";
-import { PuckAction, createReducer, replaceAction } from "@/core/reducer";
-import { InputOrGroup } from "@/core/components/InputOrGroup";
-import { AppState, ComponentConfig, Config, Data } from "@/core/types/Config";
-import { rootDroppableId } from "@/core/lib/root-droppable-id";
+import { Puck } from "@/core/components/Puck";
+
+import { ComponentConfig } from "@/core/types/Config";
 import { getClassNameFactory } from "@/core/lib";
-import { useResolvedData } from "@/core/lib/use-resolved-data";
 
 import styles from "./styles.module.css";
+import { usePuck } from "@/core/lib/use-puck";
 
-const getClassNamePreview = getClassNameFactory("Preview", styles);
+const getClassNamePreview = getClassNameFactory("PreviewFrame", styles);
 const getClassNameConfigPreview = getClassNameFactory("ConfigPreview", styles);
 
-const PreviewApp = ({
+export const PreviewFrame = ({
   children,
-  config,
-  data,
+  label,
+  style = {},
+  disableOnClick = false,
 }: {
-  children: (
-    appState: AppState,
-    dispatch: (action: PuckAction) => void,
-    componentState: Record<string, { loading: boolean }>
-  ) => ReactNode;
-  config: Config;
-  data: Data;
+  children?: ReactNode;
+  label?: string;
+  style?: CSSProperties;
+  disableOnClick?: boolean;
 }) => {
-  const [reducer] = useState(() => createReducer({ config }));
-  const [appState, dispatch] = useReducer(reducer, {
-    ...defaultAppState,
-    data,
-  });
-
-  const { componentState } = useResolvedData(appState, config, dispatch);
+  const { dispatch } = usePuck();
 
   return (
-    <AppProvider
-      value={{
-        state: appState,
-        dispatch,
-        config,
-        componentState: {},
+    <div
+      className={getClassNamePreview()}
+      onClick={() => {
+        if (disableOnClick) return;
+
+        dispatch({ type: "setUi", ui: { itemSelector: null } });
       }}
     >
-      {children(appState, dispatch, componentState)}
-    </AppProvider>
+      <div className={getClassNamePreview("header")}>
+        <div className={getClassNamePreview("annotation")}>
+          Interactive Demo
+        </div>
+        {label && <div className={getClassNamePreview("label")}>{label}</div>}
+      </div>
+      <div className={getClassNamePreview("body")} style={style}>
+        {children}
+      </div>
+    </div>
   );
 };
 
-export const Preview = ({
-  children,
-  config = { components: {} },
-  data,
+export const PuckPreview = ({
   label,
-  render,
-  padding = 16,
-}: {
-  children?: ReactNode;
-  config?: Config;
-  data?: Data;
+  children,
+  style = {},
+  ...puckProps
+}: React.ComponentProps<typeof Puck> & {
   label: string;
-  render?: (
-    appState: AppState,
-    dispatch: (action: PuckAction) => void
-  ) => ReactNode;
-  padding?: number;
+  children: ReactNode;
+  style?: CSSProperties;
 }) => {
   return (
-    <PreviewApp config={config} data={data}>
-      {(appState, dispatch) => (
-        <div className={getClassNamePreview()}>
-          {label && <div className={getClassNamePreview("label")}>{label}</div>}
-          <div
-            style={{
-              padding,
-            }}
-          >
-            {typeof render !== "undefined" ? render(appState, dispatch) : null}
-            {children}
-          </div>
+    <Puck {...puckProps}>
+      <PreviewFrame label={label} style={style}>
+        {children}
+      </PreviewFrame>
+    </Puck>
+  );
+};
+
+const ConfigPreviewInner = ({
+  componentConfig,
+}: {
+  componentConfig: ComponentConfig;
+}) => {
+  const { appState } = usePuck();
+
+  return (
+    <div className={getClassNameConfigPreview()}>
+      <div className={getClassNameConfigPreview("field")}>
+        <Puck.Fields />
+      </div>
+
+      {componentConfig.render && (
+        <div className={getClassNameConfigPreview("preview")}>
+          {componentConfig.render({
+            ...appState.data["content"][0].props,
+            puck: { renderDropZone: () => <div /> },
+          })}
         </div>
       )}
-    </PreviewApp>
+    </div>
   );
 };
 
@@ -97,7 +102,7 @@ export const ConfigPreview = ({
   label: string;
 }) => {
   return (
-    <Preview
+    <Puck
       config={{ components: { Example: componentConfig } }}
       data={{
         content: [
@@ -108,78 +113,12 @@ export const ConfigPreview = ({
         ],
         root: { props: {} },
       }}
-      label={label}
-      padding={0}
-      render={(appState, dispatch) => (
-        <div className={getClassNameConfigPreview()}>
-          <div className={getClassNameConfigPreview("field")}>
-            {Object.keys(componentConfig.fields).map((name) => (
-              <InputOrGroup
-                key={name}
-                name={name}
-                field={componentConfig.fields[name]}
-                value={appState.data["content"][0].props[name]}
-                readOnly={
-                  appState.data["content"][0].readOnly &&
-                  appState.data["content"][0].readOnly[name]
-                }
-                id={`example_${name}`}
-                onChange={async (val, ui) => {
-                  const { resolveData = (data) => data } = componentConfig;
-
-                  const newData = await resolveData(
-                    {
-                      props: {
-                        ...appState.data.content[0].props,
-                        [name]: val,
-                      },
-                      type: "Example",
-                      readOnly: {},
-                    },
-                    { changed: { [name]: true } }
-                  );
-
-                  const replacedData = replaceAction(appState.data, {
-                    type: "replace",
-                    destinationIndex: 0,
-                    destinationZone: rootDroppableId,
-                    data: {
-                      ...appState.data.content[0],
-                      props: {
-                        ...appState.data.content[0].props,
-                        ...newData.props,
-                      },
-                      readOnly: {
-                        ...appState.data["content"][0].readOnly,
-                        ...newData.readOnly,
-                      },
-                    },
-                  });
-
-                  dispatch({
-                    type: "set",
-                    state: {
-                      ui: {
-                        ...appState.ui,
-                        ...ui,
-                      },
-                      data: replacedData,
-                    },
-                  });
-                }}
-              />
-            ))}
-          </div>
-          {componentConfig.render && (
-            <div className={getClassNameConfigPreview("preview")}>
-              {componentConfig.render({
-                ...appState.data["content"][0].props,
-                puck: { renderDropZone: () => <div /> },
-              })}
-            </div>
-          )}
-        </div>
-      )}
-    />
+      onPublish={() => {}}
+      ui={{ itemSelector: { index: 0 } }}
+    >
+      <PreviewFrame label={label} style={{ padding: 0 }} disableOnClick>
+        <ConfigPreviewInner componentConfig={componentConfig} />
+      </PreviewFrame>
+    </Puck>
   );
 };
