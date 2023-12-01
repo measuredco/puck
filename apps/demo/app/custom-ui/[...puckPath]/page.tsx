4954cbbb710fece37d33e6abc51908b87fc5e922
Chris Villa <chrisvilla@me.com>
2023-12-01 13:42:52 +0000

    docs: add custom UI demo

---
+++ apps/demo/app/custom-ui/[...puckPath]/page.tsx
@@ -0,0 +1,35 @@
+import dynamic from "next/dynamic";
+import resolvePuckPath from "../../../lib/resolve-puck-path";
+import { Metadata } from "next";
+
+const Client = dynamic(() => import("./client"), {
+  ssr: false,
+});
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { framework: string; uuid: string; puckPath: string[] };
+}): Promise<Metadata> {
+  const { isEdit, path } = resolvePuckPath(params.puckPath);
+
+  if (isEdit) {
+    return {
+      title: "Editing: " + path,
+    };
+  }
+
+  return {
+    title: "",
+  };
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { framework: string; uuid: string; puckPath: string[] };
+}) {
+  const { isEdit, path } = resolvePuckPath(params.puckPath);
+
+  return <Client isEdit={isEdit} path={path} />;
+}
