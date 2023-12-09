ccf34446de177934c189c2c245eeeec5f85e265c
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2023-12-09 10:33:02 +0000

    Remix recipe: Update readme to reflect moving coonfig file, and fix a syntax error that broke linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,5 @@ module.exports = {
     "react-hooks/exhaustive-deps": "off",
   },
   // eslint-config-next causes warning on Remix's default remix.config.js
-  exclude: ["recipes/remix/remix.config.js"],
+  ignorePatterns: ["recipes/remix/remix.config.js"],
 };
