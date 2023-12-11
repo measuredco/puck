7a835242d1a0b1d1831c24f9751954e2889a8009
Chris Villa <chrisvilla@me.com>
2023-12-11 19:42:50 +0000

    docs: add versioned docs

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import type { AppProps } from "next/app";
 import "../styles.css";
+import { useRouter } from "next/router";
+import { Message } from "./v/[[...fullPath]]";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function DocsApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!window.parent) return;
+
+    const message: Message = {
+      type: "routeChange",
+      title: window.document.title,
+    };
+
+    window.parent.postMessage(message);
+
+    const handleRouteChange = (url) => {
+      const message: Message = {
+        type: "routeChange",
+        url,
+        title: window.document.title,
+      };
+
+      window.parent.postMessage(message);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
---
+++ apps/docs/pages/v/[[...fullPath]].tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from "react";
+
+export type Message = {
+  type: "routeChange";
+  url?: string;
+  title: string;
+};
+
+export default function Version({ path, version }) {
+  useEffect(() => {
+    const handleMessageReceived = (event: MessageEvent) => {
+      if (event.data.type === "routeChange") {
+        const routeChange = event.data as Message;
+
+        if (routeChange.url) {
+          window.history.pushState({}, "", `/v/${version}${routeChange.url}`);
+        }
+
+        window.document.title = `${routeChange.title} [${
+          version !== "canary" ? `v${version}` : version
+        }]`;
+      }
+    };
+
+    window.addEventListener("message", handleMessageReceived);
+
+    return () => window.removeEventListener("message", handleMessageReceived);
+  }, []);
+
+  const versionSlug = version.replace(/\./g, "");
+
+  const src =
+    version === "canary"
+      ? `https://puck-docs-git-canary-measured.vercel.app`
+      : `https://puck-docs-git-releases-v${versionSlug}-measured.vercel.app`;
+
+  return (
+    <iframe
+      src={src}
+      style={{
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        position: "fixed",
+        border: 0,
+      }}
+    ></iframe>
+  );
+}
+
+export function getServerSideProps(ctx) {
+  const [version, ...path] = ctx.query.fullPath;
+
+  return { props: { path: path.join("/"), version } };
+}
