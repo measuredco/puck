236e3665333074dbb67079f7b8ae26ab12045ac9
Chris Villa <chrisvilla@me.com>
2023-12-14 11:10:51 +0000

    chore: enable cors for the releases API

--- apps/docs/pages/api/releases.ts
+++ apps/docs/pages/api/releases.ts
@@ -9,5 +9,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
   res.status(200).json({ releases });
 }
