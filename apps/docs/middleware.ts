2a62cd5b437072540080b80087e57b73ea692b31
Chris Villa <chrisvilla@me.com>
2023-12-19 14:18:17 +0000

    docs: use proxies for versioned docs instead of iframes

---
+++ apps/docs/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import releases from "./releases.json";
+
+const versionPattern = /\/v\/([\d+|\.]+)/;
+
+export function middleware(request: NextRequest) {
+  const path = `${request.nextUrl.pathname}${request.nextUrl.search}`;
+
+  const urlMatch = versionPattern.exec(request.url);
+
+  if (urlMatch) {
+    const version = urlMatch[1];
+    const newUrl = `${releases[version]}${path}`;
+
+    return NextResponse.rewrite(new URL(newUrl));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/v/:path*"],
+};
--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,14 @@
+const packageJson = require("./package.json");
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra({ transpilePackages: ["@measured/puck"] });
+const BRANCH_NAME = process.env.VERCEL_GIT_COMMIT_REF || "";
+const IS_RELEASE_BRANCH = BRANCH_NAME.startsWith("releases/");
+
+module.exports = withNextra({
+  transpilePackages: ["@measured/puck"],
+  basePath: IS_RELEASE_BRANCH ? `/v/${packageJson.version}` : "",
+});
