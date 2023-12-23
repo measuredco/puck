a84e06feec977bca1ac7e08b6e55ba8afe0141dc
Chris Villa <chrisvilla@me.com>
2023-12-23 10:50:58 +0000

    fix: replace crypto with uuid lib

--- packages/core/lib/generate-id.ts
+++ packages/core/lib/generate-id.ts
@@ -1,4 +1,3 @@
-import { randomBytes } from "crypto";
+import { v4 as uuidv4 } from "uuid";
 
-export const generateId = (type: string | number) =>
-  `${type}-${randomBytes(20).toString("hex")}`;
+export const generateId = (type: string | number) => `${type}-${uuidv4()}`;
