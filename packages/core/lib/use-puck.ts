1b907cba506dda7a2b1fe201a426e1c4bcfffecc
prashanthvdckap <96864283+prashanthvdckap@users.noreply.github.com>
2024-02-20 13:57:20 +0530

    feat: expose history via usePuck hook

--- packages/core/lib/use-puck.ts
+++ packages/core/lib/use-puck.ts
@@ -1,7 +1,7 @@
 import { useAppContext } from "../components/Puck/context";
 
 export const usePuck = () => {
-  const { state: appState, config, dispatch } = useAppContext();
+  const { state: appState, config, history, dispatch } = useAppContext();
 
-  return { appState, config, dispatch };
+  return { appState, config, dispatch, history };
 };
