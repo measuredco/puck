68f97ae53609530129415d6c04656f961da57e29
Jon Vivian <2586037+4leite@users.noreply.github.com>
2024-03-25 20:28:14 +1300

    build: upgrade tsup and use native support for CSS modules
    tsup now processes css pre-processes natively
    this is also resolves core yarn build --watch not refreshing css changes
    see https://github.com/egoist/tsup/issues/536#issuecomment-1752121594

--- packages/tsup-config/index.ts
+++ packages/tsup-config/index.ts
@@ -1,70 +1,10 @@
-import fs from "fs";
-import path from "path";
-import postcss from "postcss";
-import postcssModules from "postcss-modules";
-
 const config = {
   dts: true,
   format: "cjs",
   inject: ["../tsup-config/react-import.js"],
-  esbuildPlugins: [
-    {
-      name: "css-module",
-      setup(build): void {
-        build.onResolve(
-          { filter: /\.module\.css$/, namespace: "file" },
-          (args) => ({
-            path: `${path.join(args.resolveDir, args.path)}#css-module`,
-            namespace: "css-module",
-            pluginData: {
-              pathDir: path.join(args.resolveDir, args.path),
-            },
-          })
-        );
-        build.onLoad(
-          { filter: /#css-module$/, namespace: "css-module" },
-          async (args) => {
-            const { pluginData } = args as {
-              pluginData: { pathDir: string };
-            };
-
-            const source = fs.readFileSync(pluginData.pathDir, "utf8");
-
-            let cssModule = {};
-            const result = await postcss([
-              postcssModules({
-                getJSON(_, json) {
-                  cssModule = json;
-                },
-              }),
-            ]).process(source, { from: pluginData.pathDir });
-
-            return {
-              pluginData: { css: result.css },
-              contents: `import "${
-                pluginData.pathDir
-              }"; export default ${JSON.stringify(cssModule)}`,
-            };
-          }
-        );
-        build.onResolve(
-          { filter: /\.module\.css$/, namespace: "css-module" },
-          (args) => ({
-            path: path.join(args.resolveDir, args.path, "#css-module-data"),
-            namespace: "css-module",
-            pluginData: args.pluginData as { css: string },
-          })
-        );
-        build.onLoad(
-          { filter: /#css-module-data$/, namespace: "css-module" },
-          (args) => ({
-            contents: (args.pluginData as { css: string }).css,
-            loader: "css",
-          })
-        );
-      },
-    },
-  ],
+  loader: {
+    ".css": "local-css",
+  },
 };
 
 export default config;
