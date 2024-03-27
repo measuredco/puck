ea2a3d019d958dafa9bc4b98bfa9497cda564d85
Chris Villa <chrisvxd@users.noreply.github.com>
2024-03-27 15:45:28 +0100

    chore: prevent iOS crashes when dragging in large viewport
    Disabling animations prevents a "Safari encountered a problem" error
    when dragging an item on a desktop viewport on iOS Safari, which was
    likely due to a GPU memory issue.

---
+++ packages/core/lib/is-ios.ts
@@ -0,0 +1,11 @@
+export const isIos = () =>
+  [
+    "iPad Simulator",
+    "iPhone Simulator",
+    "iPod Simulator",
+    "iPad",
+    "iPhone",
+    "iPod",
+  ].includes(navigator.platform) ||
+  // iPad on iOS 13 detection
+  (navigator.userAgent.includes("Mac") && "ontouchend" in document);
