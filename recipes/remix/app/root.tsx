0898b26cd021680dfb77a439b04140ce2fb8cb2c
Chris Villa <chrisvilla@me.com>
2024-08-31 11:23:06 +0100

    fix: remove body margin in remix recipe

--- recipes/remix/app/root.tsx
+++ recipes/remix/app/root.tsx
@@ -9,8 +9,11 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "~/styles/shared.css";
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
 ];
 
 export default function App() {
