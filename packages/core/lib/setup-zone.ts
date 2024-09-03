e4cca94a9dc2b3d42ae1909f3458ef81092d558d
Chris Villa <chrisvilla@me.com>
2024-09-03 19:01:03 +0100

    refactor: simplify zone type forcing

--- packages/core/lib/setup-zone.ts
+++ packages/core/lib/setup-zone.ts
@@ -4,16 +4,6 @@ import { rootDroppableId } from "./root-droppable-id";
 // Force 'zones' to always be present and non-undefined
 type WithZones<T extends Data> = T & { zones: NonNullable<T["zones"]> };
 
-// Ensuring zones is non-undefined and part of the final type
-function ensureZones<UserData extends Data>(
-  data: UserData
-): WithZones<UserData> {
-  return {
-    ...data,
-    zones: data.zones || {},
-  } as WithZones<UserData>;
-}
-
 export const setupZone = <UserData extends Data>(
   data: UserData,
   zoneKey: string
@@ -23,7 +13,10 @@ export const setupZone = <UserData extends Data>(
   }
 
   // Preprocess to ensure zones is not undefined
-  const newData = ensureZones(data);
+  const newData = {
+    ...data,
+    zones: data.zones || {},
+  };
 
   newData.zones[zoneKey] = newData.zones[zoneKey] || [];
 
