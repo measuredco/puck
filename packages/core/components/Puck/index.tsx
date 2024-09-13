cd4438eb43cb22a129fe491c1f2c8c21ed6b8799
Chris Villa <chrisvilla@me.com>
2024-09-13 17:16:26 +0100

    refactor: default iframe config correctly

--- packages/core/components/Puck/index.tsx
+++ packages/core/components/Puck/index.tsx
@@ -87,9 +87,7 @@ export function Puck<
   headerTitle,
   headerPath,
   viewports = defaultViewports,
-  iframe = {
-    enabled: true,
-  },
+  iframe: _iframe,
   dnd,
   initialHistory: _initialHistory,
 }: {
@@ -121,6 +119,11 @@ export function Puck<
   };
   initialHistory?: InitialHistory;
 }) {
+  const iframe: IframeConfig = {
+    enabled: true,
+    ..._iframe,
+  };
+
   const [generatedAppState] = useState<AppState<UserData>>(() => {
     const initial = { ...defaultAppState.ui, ...initialUi };
 
