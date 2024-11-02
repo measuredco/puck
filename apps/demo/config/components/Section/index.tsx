464da9873b09226b7a0ad6a6ed01a894a860fce8
Chris Villa <chrisvilla@me.com>
2024-11-02 21:12:47 +0000

    docs: update demo to use new dnd engine

--- apps/demo/config/components/Section/index.tsx
+++ apps/demo/config/components/Section/index.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, ReactNode } from "react";
+import { CSSProperties, forwardRef, ReactNode } from "react";
 import styles from "./styles.module.css";
 import { getClassNameFactory } from "@/core/lib";
 
@@ -7,30 +7,24 @@ const getClassName = getClassNameFactory("Section", styles);
 export type SectionProps = {
   className?: string;
   children: ReactNode;
-  padding?: string;
   maxWidth?: string;
   style?: CSSProperties;
 };
 
-export const Section = ({
-  children,
-  className,
-  padding = "0px",
-  maxWidth = "1280px",
-  style = {},
-}: SectionProps) => {
-  return (
-    <div
-      className={`${getClassName()}${className ? ` ${className}` : ""}`}
-      style={{
-        ...style,
-        paddingTop: padding,
-        paddingBottom: padding,
-      }}
-    >
-      <div className={getClassName("inner")} style={{ maxWidth }}>
-        {children}
+export const Section = forwardRef<HTMLDivElement, SectionProps>(
+  ({ children, className, maxWidth = "1280px", style = {} }, ref) => {
+    return (
+      <div
+        className={`${getClassName()}${className ? ` ${className}` : ""}`}
+        style={{
+          ...style,
+        }}
+        ref={ref}
+      >
+        <div className={getClassName("inner")} style={{ maxWidth }}>
+          {children}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
