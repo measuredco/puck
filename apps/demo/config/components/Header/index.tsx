485b434e2749c0071a0b0d2e59a54fb2e05302af
gregpalaci <gregory.palaci.uk@gmail.com>
2024-12-13 21:23:02 +0000

    feature: quick experiment Viewport previewing #72
    just a quick demo

--- apps/demo/config/components/Header/index.tsx
+++ apps/demo/config/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { getClassNameFactory } from "@/core/lib";
 
 import styles from "./styles.module.css";
+import html2canvas from "html2canvas";
 
 const getClassName = getClassNameFactory("Header", styles);
 
@@ -34,6 +35,26 @@ const Header = ({ editMode }: { editMode: boolean }) => (
       <NavItem label="Home" href={`${editMode ? "" : "/"}`} />
       <NavItem label="Pricing" href={editMode ? "" : "/pricing"} />
       <NavItem label="About" href={editMode ? "" : "/about"} />
+      <div
+        onClick={() => {
+          html2canvas(document.body).then((canvas) => {
+            const dialog = document.createElement("dialog");
+            dialog.id = "preview-puck";
+            document.body.appendChild(dialog);
+            dialog.showModal();
+            dialog.appendChild(canvas);
+
+            dialog.addEventListener("click", () => {
+              dialog.close();
+            });
+            dialog.addEventListener("keydown", (e) => {
+              dialog.close();
+            });
+          });
+        }}
+      >
+        Export a Preview
+      </div>
     </nav>
   </header>
 );
