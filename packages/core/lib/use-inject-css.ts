df70077c654241a3d287e6181e8dc76b10ca9e80
Chris Villa <chrisvilla@me.com>
2024-12-14 18:15:44 +0000

    refactor: move drag styles into separate file, but retain injection hook for future

--- packages/core/lib/use-inject-css.ts
+++ packages/core/lib/use-inject-css.ts
@@ -1,45 +1,7 @@
 import { useEffect, useState } from "react";
 import { getFrame } from "./get-frame";
 
-const styles = `
-/* Prevent user from interacting with underlying component */
-[data-puck-component] * {
-  pointer-events: none;
-  user-select: none;
-    -webkit-user-select: none;
-}
-
-[data-puck-component] {
-  cursor: grab;
-  pointer-events: auto !important;
-  user-select: none;
-  -webkit-user-select: none;
-}
-
-[data-puck-disabled] {
-  cursor: pointer;
-}
-
-/* Placeholder */
-[data-puck-dragging]:not([data-dnd-dragging]) {
-  background: var(--puck-color-azure-06) !important;
-  border: none !important;
-  color: #00000000 !important;
-  opacity: 0.3 !important;
-  outline: none !important;
-  transition: none !important;
-}
-
-[data-puck-dragging]:not([data-dnd-dragging]) *, [data-puck-dragging]:not([data-dnd-dragging])::after, [data-puck-dragging]:not([data-dnd-dragging])::before {
-  opacity: 0 !important;
-}
-
-[data-dnd-dragging] {
-  pointer-events: none !important;
-  outline: 2px var(--puck-color-azure-09) solid !important;
-  outline-offset: -2px !important;
-}
-`;
+const styles = ``;
 
 export const useInjectStyleSheet = (
   initialStyles: string,
