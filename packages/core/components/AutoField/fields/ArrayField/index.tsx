874ba1b9d39aed5da09103cfcfe942c94560c266
Chris Villa <chrisvilla@me.com>
2025-01-10 10:51:07 +0000

    fix: use correct label for array and object subfields

--- packages/core/components/AutoField/fields/ArrayField/index.tsx
+++ packages/core/components/AutoField/fields/ArrayField/index.tsx
@@ -272,14 +272,20 @@ export const ArrayField = ({
                               "undefined"
                             ? readOnlyFields[subFieldName]
                             : readOnlyFields[wildcardFieldName];
+
+                          const label = subField.label || fieldName;
+
                           return (
                             <AutoFieldPrivate
                               key={subFieldName}
                               name={subFieldName}
-                              label={subField.label || fieldName}
+                              label={label}
                               id={`${_arrayId}_${fieldName}`}
                               readOnly={subReadOnly}
-                              field={subField}
+                              field={{
+                                ...subField,
+                                label, // May be used by custom fields
+                              }}
                               value={data[fieldName]}
                               onChange={(val, ui) => {
                                 onChange(
