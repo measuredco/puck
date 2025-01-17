12c8643f2da1d8458a66fde4a1f8bfa7a967b491
Chris Villa <chrisvilla@me.com>
2025-01-17 11:35:25 +0000

    refactor: extract useResolvedFields into lib

--- packages/core/components/Puck/components/Fields/index.tsx
+++ packages/core/components/Puck/components/Fields/index.tsx
@@ -6,25 +6,17 @@ import {
   replaceAction,
   setAction,
 } from "../../../../reducer";
-import { ComponentData, RootData, UiState } from "../../../../types";
-import type { Field, Fields as FieldsType } from "../../../../types";
+import { UiState } from "../../../../types";
 import { AutoFieldPrivate } from "../../../AutoField";
 import { useAppContext } from "../../context";
 
 import styles from "./styles.module.css";
 import { getClassNameFactory } from "../../../../lib";
-import { ReactNode, useCallback, useMemo, useRef, useState } from "react";
+import { ReactNode, useMemo } from "react";
 import { ItemSelector } from "../../../../lib/get-item";
-import { getChanged } from "../../../../lib/get-changed";
-import { useParent } from "../../../../lib/use-parent";
-import { useOnValueChange } from "../../../../lib/use-on-value-change";
-import { selectorIs } from "../../../../lib/selector-is";
+import { useResolvedFields } from "../../../../lib/use-resolved-fields";
 
 const getClassName = getClassNameFactory("PuckFields", styles);
-
-const defaultPageFields: Record<string, Field> = {
-  title: { type: "text" },
-};
 
 const DefaultFields = ({
   children,
@@ -34,144 +26,6 @@ const DefaultFields = ({
   itemSelector?: ItemSelector | null;
 }) => {
   return <>{children}</>;
-};
-
-type ComponentOrRootData = Omit<ComponentData<any>, "type">;
-
-const useResolvedFields = (): [FieldsType, boolean] => {
-  const { selectedItem, state, config } = useAppContext();
-  const parent = useParent();
-
-  const { data } = state;
-
-  const rootFields = config.root?.fields || defaultPageFields;
-
-  const componentConfig = selectedItem
-    ? config.components[selectedItem.type]
-    : null;
-
-  const defaultFields = useMemo(
-    () =>
-      (selectedItem
-        ? (componentConfig?.fields as Record<string, Field<any>>)
-        : rootFields) || {},
-    [selectedItem, rootFields, componentConfig?.fields]
-  );
-
-  // DEPRECATED
-  const rootProps = data.root.props || data.root;
-
-  const [lastSelectedData, setLastSelectedData] = useState<
-    Partial<ComponentOrRootData>
-  >({});
-  const [resolvedFields, setResolvedFields] = useState(defaultFields);
-  const [fieldsLoading, setFieldsLoading] = useState(false);
-  const lastFields = useRef<FieldsType>({});
-
-  const defaultResolveFields = (
-    _componentData: ComponentOrRootData,
-    _params: {
-      fields: FieldsType;
-      lastData: Partial<ComponentOrRootData> | null;
-      lastFields: FieldsType;
-      changed: Record<string, boolean>;
-    }
-  ) => defaultFields;
-
-  const componentData: ComponentOrRootData = selectedItem
-    ? selectedItem
-    : { props: rootProps, readOnly: data.root.readOnly };
-
-  const hasComponentResolver = selectedItem && componentConfig?.resolveFields;
-  const hasRootResolver = !selectedItem && config.root?.resolveFields;
-  const hasResolver = hasComponentResolver || hasRootResolver;
-
-  const resolveFields = useCallback(
-    async (fields: FieldsType = {}) => {
-      const lastData =
-        lastSelectedData.props?.id === componentData.props.id
-          ? lastSelectedData
-          : null;
-
-      const changed = getChanged(componentData, lastData);
-
-      setLastSelectedData(componentData);
-
-      if (hasComponentResolver) {
-        return await componentConfig!.resolveFields!(
-          componentData as ComponentData,
-          {
-            changed,
-            fields,
-            lastFields: lastFields.current,
-            lastData: lastData as ComponentData,
-            appState: state,
-            parent,
-          }
-        );
-      }
-
-      if (hasRootResolver) {
-        return await config.root!.resolveFields!(componentData, {
-          changed,
-          fields,
-          lastFields: lastFields.current,
-          lastData: lastData as RootData,
-          appState: state,
-          parent,
-        });
-      }
-
-      return defaultResolveFields(componentData, {
-        changed,
-        fields,
-        lastFields: lastFields.current,
-        lastData,
-      });
-    },
-    [data, config, componentData, selectedItem, state, parent]
-  );
-
-  const triggerResolver = useCallback(() => {
-    // Must either be in default zone, or have parent
-    if (
-      !state.ui.itemSelector?.zone ||
-      state.ui.itemSelector?.zone === "default-zone" ||
-      parent
-    ) {
-      if (hasResolver) {
-        setFieldsLoading(true);
-
-        resolveFields(defaultFields).then((fields) => {
-          setResolvedFields(fields || {});
-
-          setFieldsLoading(false);
-        });
-
-        return;
-      }
-    }
-
-    setResolvedFields(defaultFields);
-  }, [defaultFields, state.ui.itemSelector, hasResolver, parent]);
-
-  useOnValueChange<ItemSelector | null>(
-    state.ui.itemSelector,
-    () => {
-      lastFields.current = defaultFields;
-    },
-    selectorIs
-  );
-
-  useOnValueChange(
-    { data, parent, itemSelector: state.ui.itemSelector },
-    () => {
-      triggerResolver();
-    },
-    (a, b) => JSON.stringify(a) === JSON.stringify(b)
-  );
-
-  return [resolvedFields, fieldsLoading];
 };
 
 export const Fields = ({ wrapFields = true }: { wrapFields?: boolean }) => {
