b554d0a5e9d3fec55a861e65e294d6f55d69f3d3
christian-tchaikovsky <tchaikovsky.md@gmail.com>
2025-04-10 12:49:02 +0300

    style: fix formatting

--- packages/core/lib/get-component-config.ts
+++ packages/core/lib/get-component-config.ts
@@ -2,7 +2,10 @@ import { Config, DefaultComponentProps } from "../types";
 
 export const getComponentConfig = <
   Props extends DefaultComponentProps = DefaultComponentProps
->(config: Config<Props>, type: string | number) => {
+>(
+  config: Config<Props>,
+  type: string | number
+) => {
   const component = config.components[type];
 
   return typeof component === "function"
