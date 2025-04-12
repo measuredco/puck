7d2ba69cc636b9bba79bb32de6b0ba535cbdc221
Chris Villa <chrisvilla@me.com>
2025-04-12 09:24:01 +0100

    refactor: resolve all slot children on component change

--- packages/core/lib/map-slots.ts
+++ packages/core/lib/map-slots.ts
@@ -1,22 +1,20 @@
 import { ComponentData, Content, RootData } from "../types";
 import { forEachSlot } from "../lib/for-each-slot";
 
-export function mapSlots<T extends ComponentData | RootData>(
+export async function mapSlots<T extends ComponentData | RootData>(
   item: T,
-  map: (data: Content, path: string[]) => Content,
-  path: string[] = []
-): T {
+  map: (data: Content, propName: string) => Promise<Content>
+): Promise<T> {
   const props: Record<string, any> = { ...item.props };
 
-  forEachSlot(item, (_parentId, propName, content) => {
-    const currentPath = [...path, `${_parentId}:${propName}`];
-
-    props[propName] = map(
-      content.map((item) => {
-        return mapSlots(item, map as any, currentPath);
-      }),
-      currentPath
+  await forEachSlot(item, async (_parentId, propName, content) => {
+    const mappedContent = await Promise.all(
+      content.map(async (item) => {
+        return await mapSlots(item, map as any);
+      })
     );
+
+    props[propName] = await map(mappedContent, propName);
   });
 
   return { ...item, props };
