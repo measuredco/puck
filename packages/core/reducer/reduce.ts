bf6609d157d4b0438b3b5e122565a26b2df2ba2d
Chris Villa <chrisvilla@me.com>
2025-04-15 22:20:21 +0100

    refactor: throw error if user changes id during replace operation

--- packages/core/reducer/reduce.ts
+++ packages/core/reducer/reduce.ts
@@ -151,10 +151,31 @@ export const replaceAction = <UserData extends Data>(
   const [parentId] = action.destinationZone.split(":");
   const idsInPath = getIdsForParent(action.destinationZone, state);
 
+  const originalId =
+    state.indexes.zones[action.destinationZone].contentIds[
+      action.destinationIndex
+    ];
+
+  const idChanged = originalId !== action.data.props.id;
+
+  if (idChanged) {
+    throw new Error(
+      'Can\'t change the id during a replace action. Please us "remove" and "insert" to define a new node.'
+    );
+  }
+
   return walkTree<UserData>(
     state,
     appStore.config,
-    (content) => content,
+    (content, zoneCompound) => {
+      const newContent = [...content];
+
+      if (zoneCompound === action.destinationZone) {
+        newContent[action.destinationIndex] = action.data;
+      }
+
+      return newContent;
+    },
     (childItem, path) => {
       const pathIds = path.map((p) => p.split(":")[0]);
 
