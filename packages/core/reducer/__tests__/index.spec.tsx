2b4ac336c763fb625e2a172e236c83ae83b9a46f
Chris Villa <chrisvilla@me.com>
2025-04-15 12:14:22 +0100

    test: add initial reducer tests, covering insert

--- packages/core/reducer/__tests__/index.spec.tsx
+++ packages/core/reducer/__tests__/index.spec.tsx
@@ -2,19 +2,26 @@ import {
   DuplicateAction,
   InsertAction,
   MoveAction,
+  PuckAction,
   RegisterZoneAction,
   RemoveAction,
   ReorderAction,
   ReplaceAction,
   SetDataAction,
+  SetUiAction,
   UnregisterZoneAction,
   createReducer,
 } from "../../reducer";
-import { AppState, Config, Data, UiState } from "../../types";
+import { ComponentData, Config, Data, Slot, UiState } from "../../types";
 import { rootDroppableId } from "../../lib/root-droppable-id";
 
 import { generateId } from "../../lib/generate-id";
-import { defaultAppState } from "../../store";
+import {
+  createAppStore,
+  defaultAppState as _defaultAppState,
+} from "../../store";
+import { PrivateAppState } from "../../types/Internal";
+import { stripSlots } from "../../lib/strip-slots";
 
 jest.mock("../../lib/generate-id");
 
@@ -23,69 +30,224 @@ const mockedGenerateId = generateId as jest.MockedFunction<typeof generateId>;
 type Props = {
   Comp: {
     prop: string;
+    slot: Slot;
   };
 };
 
-type UserConfig = Config<Props>;
-
-const defaultData: Data = {
-  root: { props: { title: "" } },
-  content: [],
-  zones: {},
+type RootProps = {
+  title: string;
+  slot: Slot;
 };
 
-const defaultUi: UiState = defaultAppState.ui;
+type UserConfig = Config<Props, RootProps>;
+type UserData = Data<Props, RootProps>;
 
-describe("Data reducer", () => {
+const dzZoneCompound = "my-component:zone1";
+
+const defaultData: UserData = {
+  root: { props: { title: "", slot: [] } },
+  content: [],
+  zones: { [dzZoneCompound]: [] },
+};
+
+const defaultUi: UiState = _defaultAppState.ui;
+
+const defaultIndexes: PrivateAppState<UserData>["indexes"] = {
+  nodes: {},
+  zones: {
+    "root:slot": { contentIds: [], type: "slot" },
+    [dzZoneCompound]: { contentIds: [], type: "dropzone" },
+  },
+};
+
+const defaultState = {
+  data: defaultData,
+  ui: defaultUi,
+  indexes: defaultIndexes,
+};
+
+const appStore = createAppStore();
+
+const expectIndexed = (
+  state: PrivateAppState,
+  item: ComponentData | undefined,
+  path: string[],
+  index: number
+) => {
+  if (!item) return;
+
+  const zoneCompound = path[path.length - 1];
+
+  expect(state.indexes.zones[zoneCompound].contentIds[index]).toEqual(
+    item.props.id
+  );
+  expect(state.indexes.nodes[item.props.id].data).toEqual(item);
+  expect(state.indexes.nodes[item.props.id].flatData).toEqual(stripSlots(item));
+  expect(state.indexes.nodes[item.props.id].path).toEqual(path);
+};
+
+describe("Reducer", () => {
   const config: UserConfig = {
+    root: {
+      fields: { title: { type: "text" }, slot: { type: "slot" } },
+    },
     components: {
       Comp: {
-        defaultProps: { prop: "example" },
+        fields: {
+          prop: { type: "text" },
+          slot: { type: "slot" },
+        },
+        defaultProps: { prop: "example", slot: [] },
         render: () => <div />,
       },
     },
   };
 
-  const reducer = createReducer({ config });
+  let reducer = createReducer({ appStore: appStore.getState() });
 
-  describe("insert action", () => {
-    it("should insert into rootDroppableId", () => {
-      const state: AppState = { ui: defaultUi, data: { ...defaultData } };
+  beforeEach(() => {
+    appStore.setState(
+      {
+        ...appStore.getInitialState(),
+        config,
+      },
+      true
+    );
 
-      const action: InsertAction = {
-        type: "insert",
-        componentType: "Comp",
-        destinationIndex: 0,
-        destinationZone: rootDroppableId,
-      };
+    reducer = createReducer({ appStore: appStore.getState() });
 
-      const newState = reducer(state, action);
-      expect(newState.data.content[0]).toHaveProperty("type", "Comp");
-      expect(newState.data.content[0].props).toHaveProperty("prop", "example");
+    let counter = 0;
+
+    mockedGenerateId.mockImplementation(() => `mockId-${counter++}`);
+  });
+
+  const executeSequence = (
+    initialState: PrivateAppState<UserData>,
+    actions: ((currentState: PrivateAppState<UserData>) => PuckAction)[]
+  ) => {
+    let currentState: PrivateAppState<UserData> = initialState;
+
+    actions.forEach((actionFn) => {
+      const action = actionFn(currentState);
+
+      currentState = reducer(currentState, action) as PrivateAppState<UserData>;
     });
 
-    it("should insert into a different zone", () => {
-      const state: AppState = { ui: defaultUi, data: { ...defaultData } };
-      const action: InsertAction = {
-        type: "insert",
-        componentType: "Comp",
-        destinationIndex: 0,
-        destinationZone: "zone1",
-      };
+    return currentState;
+  };
 
-      const newState = reducer(state, action);
-      expect(newState.data.zones?.zone1[0]).toHaveProperty("type", "Comp");
+  describe.only("insert action", () => {
+    describe("with DropZones", () => {
+      it("should insert into rootDroppableId", () => {
+        const action: InsertAction = {
+          type: "insert",
+          componentType: "Comp",
+          destinationIndex: 0,
+          destinationZone: rootDroppableId,
+        };
 
-      expect(newState.data.zones?.zone1[0].props).toHaveProperty(
-        "prop",
-        "example"
-      );
+        const newState = reducer(defaultState, action);
+        const item = newState.data.content[0];
+
+        expect(item).toHaveProperty("type", "Comp");
+        expect(item.props).toHaveProperty("prop", "example");
+        expectIndexed(newState, item, [rootDroppableId], 0);
+      });
+
+      it("should insert into a different zone", () => {
+        const action: InsertAction = {
+          type: "insert",
+          componentType: "Comp",
+          destinationIndex: 0,
+          destinationZone: dzZoneCompound,
+        };
+
+        const state = reducer(defaultState, action);
+
+        const item = state.data.zones?.[dzZoneCompound][0];
+
+        expect(item).toHaveProperty("type", "Comp");
+        expect(item?.props).toHaveProperty("prop", "example");
+        expectIndexed(state, item, [rootDroppableId, dzZoneCompound], 0);
+      });
+    });
+
+    describe("with slots", () => {
+      it("should insert into a root slot", () => {
+        const state: PrivateAppState<UserData> = defaultState;
+
+        const action: InsertAction = {
+          type: "insert",
+          componentType: "Comp",
+          destinationIndex: 0,
+          destinationZone: "root:slot",
+        };
+
+        const newState = reducer(state, action) as PrivateAppState<UserData>;
+
+        const item = newState.data.root.props?.slot[0];
+
+        expect(item).toHaveProperty("type", "Comp");
+        expect(item?.props).toHaveProperty("prop", "example");
+        expectIndexed(newState, item, ["root:slot"], 0);
+      });
+
+      it("should insert into a slot within a slot", () => {
+        const state = executeSequence(defaultState, [
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationIndex: 0,
+            destinationZone: "root:slot",
+          }),
+          (state) => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationIndex: 0,
+            destinationZone: `${state.data.root.props?.slot[0].props?.id}:slot`,
+          }),
+        ]);
+
+        const item = state.data.root.props?.slot[0]?.props.slot[0];
+
+        expect(item).toHaveProperty("type", "Comp");
+        expect(item?.props).toHaveProperty("prop", "example");
+        expectIndexed(
+          state,
+          item,
+          ["root:slot", `${state.data.root.props?.slot[0].props?.id}:slot`],
+          0
+        );
+      });
+
+      it("should insert into a slot within a DropZone", () => {
+        const state = executeSequence(defaultState, [
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationIndex: 0,
+            destinationZone: dzZoneCompound,
+          }),
+          (state) => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationIndex: 0,
+            destinationZone: `${state.data.zones?.[dzZoneCompound][0]?.props.id}:slot`,
+          }),
+        ]);
+
+        const item = state.data.zones?.[dzZoneCompound][0];
+
+        expect(item).toHaveProperty("type", "Comp");
+        expect(item?.props).toHaveProperty("prop", "example");
+        expectIndexed(state, item, [rootDroppableId, dzZoneCompound], 0);
+      });
     });
   });
 
   describe("reorder action", () => {
     it("should reorder within rootDroppableId", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -94,6 +256,7 @@ describe("Data reducer", () => {
             { type: "Comp", props: { id: "2" } },
           ],
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: ReorderAction = {
         type: "reorder",
@@ -108,7 +271,7 @@ describe("Data reducer", () => {
     });
 
     it("should reorder within a different zone", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -119,6 +282,7 @@ describe("Data reducer", () => {
             ],
           },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: ReorderAction = {
         type: "reorder",
@@ -135,7 +299,7 @@ describe("Data reducer", () => {
 
   describe("duplicate action", () => {
     it("should duplicate in content", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -146,6 +310,7 @@ describe("Data reducer", () => {
             },
           ],
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: DuplicateAction = {
         type: "duplicate",
@@ -160,7 +325,7 @@ describe("Data reducer", () => {
     });
 
     it("should duplicate in a different zone", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -173,6 +338,7 @@ describe("Data reducer", () => {
             ],
           },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: DuplicateAction = {
         type: "duplicate",
@@ -193,7 +359,7 @@ describe("Data reducer", () => {
 
       mockedGenerateId.mockImplementation(() => `mockId-${counter++}`);
 
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -212,6 +378,7 @@ describe("Data reducer", () => {
             ],
           },
         },
+        indexes: { nodes: {}, zones: {} },
       };
 
       const action: DuplicateAction = {
@@ -269,17 +436,43 @@ describe("Data reducer", () => {
         }
       `);
     });
+
+    it("should select the duplicated item", () => {
+      const state: PrivateAppState = {
+        ui: defaultUi,
+        data: {
+          ...defaultData,
+          content: [
+            {
+              type: "Comp",
+              props: { id: "sampleId", prop: "Some example data" },
+            },
+          ],
+        },
+        indexes: { nodes: {}, zones: {} },
+      };
+      const action: DuplicateAction = {
+        type: "duplicate",
+        sourceIndex: 0,
+        sourceZone: rootDroppableId,
+      };
+
+      const newState = reducer(state, action);
+      expect(newState.ui.itemSelector?.index).toBe(1);
+      expect(newState.ui.itemSelector?.zone).toBe(rootDroppableId);
+    });
   });
 
   describe("move action", () => {
     it("should move from rootDroppableId to another zone", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           content: [{ type: "Comp", props: { id: "1" } }],
           zones: { zone1: [{ type: "Comp", props: { id: "2" } }] },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: MoveAction = {
         type: "move",
@@ -295,13 +488,14 @@ describe("Data reducer", () => {
     });
 
     it("should move from a zone to rootDroppableId", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           content: [{ type: "Comp", props: { id: "1" } }],
           zones: { zone1: [{ type: "Comp", props: { id: "2" } }] },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: MoveAction = {
         type: "move",
@@ -317,7 +511,7 @@ describe("Data reducer", () => {
     });
 
     it("should move between two zones", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -327,6 +521,7 @@ describe("Data reducer", () => {
             zone2: [{ type: "Comp", props: { id: "2" } }],
           },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: MoveAction = {
         type: "move",
@@ -346,12 +541,13 @@ describe("Data reducer", () => {
     const replacement = { type: "Comp", props: { id: "3" } };
 
     it("should replace in content", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           content: [{ type: "Comp", props: { id: "1" } }],
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: ReplaceAction = {
         type: "replace",
@@ -366,12 +562,13 @@ describe("Data reducer", () => {
     });
 
     it("should replace in a zone", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           zones: { zone1: [{ type: "Comp", props: { id: "1" } }] },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: ReplaceAction = {
         type: "replace",
@@ -388,12 +585,13 @@ describe("Data reducer", () => {
 
   describe("remove action", () => {
     it("should remove from content", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           content: [{ type: "Comp", props: { id: "1" } }],
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: RemoveAction = {
         type: "remove",
@@ -406,12 +604,13 @@ describe("Data reducer", () => {
     });
 
     it("should remove from a zone", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           zones: { zone1: [{ type: "Comp", props: { id: "1" } }] },
         },
+        indexes: { nodes: {}, zones: {} },
       };
       const action: RemoveAction = {
         type: "remove",
@@ -428,7 +627,7 @@ describe("Data reducer", () => {
 
       mockedGenerateId.mockImplementation(() => `mockId-${counter++}`);
 
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
@@ -447,6 +646,7 @@ describe("Data reducer", () => {
             ],
           },
         },
+        indexes: { nodes: {}, zones: {} },
       };
 
       const action: RemoveAction = {
@@ -471,16 +671,41 @@ describe("Data reducer", () => {
         }
       `);
     });
+
+    it("should deselect the item", () => {
+      const state: PrivateAppState = {
+        ui: defaultUi,
+        data: {
+          ...defaultData,
+          content: [
+            {
+              type: "Comp",
+              props: { id: "sampleId", prop: "Some example data" },
+            },
+          ],
+        },
+        indexes: { nodes: {}, zones: {} },
+      };
+      const action: RemoveAction = {
+        type: "remove",
+        index: 0,
+        zone: rootDroppableId,
+      };
+
+      const newState = reducer(state, action);
+      expect(newState.ui.itemSelector).toBeNull();
+    });
   });
 
   describe("unregisterZone action", () => {
     it("should unregister a zone", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           zones: { zone1: [{ type: "Comp", props: { id: "1" } }] },
         },
+        indexes: { nodes: {}, zones: {} },
       };
 
       const action: UnregisterZoneAction = {
@@ -495,12 +720,13 @@ describe("Data reducer", () => {
 
   describe("registerZone action", () => {
     it("should register a zone that's been previously unregistered", () => {
-      const state: AppState = {
+      const state: PrivateAppState = {
         ui: defaultUi,
         data: {
           ...defaultData,
           zones: { zone1: [{ type: "Comp", props: { id: "1" } }] },
         },
+        indexes: { nodes: {}, zones: {} },
       };
 
       const unregisterAction: UnregisterZoneAction = {
@@ -523,7 +749,11 @@ describe("Data reducer", () => {
 
   describe("set action", () => {
     it("should set new data", () => {
-      const state: AppState = { ui: defaultUi, data: { ...defaultData } };
+      const state: PrivateAppState = {
+        ui: defaultUi,
+        data: { ...defaultData },
+        indexes: { nodes: {}, zones: {} },
+      };
       const newData: Data = {
         ...defaultData,
         root: { props: { title: "Hello, world" } },
@@ -537,6 +767,20 @@ describe("Data reducer", () => {
 
       const newState = reducer(state, action);
       expect(newState.data).toEqual(newData);
+    });
+  });
+
+  describe("setUi action", () => {
+    it("should insert data into the state", () => {
+      const state: PrivateAppState = defaultState;
+
+      const action: SetUiAction = {
+        type: "setUi",
+        ui: { leftSideBarVisible: false },
+      };
+
+      const newState = reducer(state, action);
+      expect(newState.ui.leftSideBarVisible).toEqual(false);
     });
   });
 });
