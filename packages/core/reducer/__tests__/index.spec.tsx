8d69eeb186b6b091761af61c098a3b29d7eb19d7
Chris Villa <chrisvilla@me.com>
2025-04-15 21:30:33 +0100

    test: add tests for move action

--- packages/core/reducer/__tests__/index.spec.tsx
+++ packages/core/reducer/__tests__/index.spec.tsx
@@ -775,76 +775,167 @@ describe("Reducer", () => {
   });
 
   describe("move action", () => {
-    it("should move from rootDroppableId to another zone", () => {
-      const state: PrivateAppState = {
-        ui: defaultUi,
-        data: {
-          ...defaultData,
-          content: [{ type: "Comp", props: { id: "1" } }],
-          zones: { zone1: [{ type: "Comp", props: { id: "2" } }] },
-        },
-        indexes: { nodes: {}, zones: {} },
-      };
-      const action: MoveAction = {
-        type: "move",
-        sourceIndex: 0,
-        sourceZone: rootDroppableId,
-        destinationIndex: 1,
-        destinationZone: "zone1",
-      };
+    describe("with DropZones", () => {
+      it("should reorder within rootDroppableId", () => {
+        const newState = executeSequence(defaultState, [
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: rootDroppableId,
+            destinationIndex: 0,
+            id: "1",
+          }),
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: rootDroppableId,
+            destinationIndex: 1,
+            id: "2",
+          }),
+          () => ({
+            type: "move",
+            sourceIndex: 0,
+            sourceZone: rootDroppableId,
+            destinationIndex: 1,
+            destinationZone: rootDroppableId,
+          }),
+        ]);
 
-      const newState = reducer(state, action);
-      expect(newState.data.content).toHaveLength(0);
-      expect(newState.data.zones?.zone1[1].props.id).toBe("1");
+        expect(newState.data.content[0].props.id).toBe("2");
+        expect(newState.data.content[1].props.id).toBe("1");
+        expectIndexed(newState, newState.data.content[0], [rootDroppableId], 0);
+        expectIndexed(newState, newState.data.content[1], [rootDroppableId], 1);
+      });
+
+      it("should move items between zones", () => {
+        const newState = executeSequence(defaultState, [
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: dzZoneCompound,
+            destinationIndex: 0,
+            id: "1",
+          }),
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: dzZoneCompound,
+            destinationIndex: 1,
+            id: "2",
+          }),
+          () => ({
+            type: "move",
+            sourceIndex: 1,
+            sourceZone: dzZoneCompound,
+            destinationIndex: 0,
+            destinationZone: rootDroppableId,
+          }),
+        ]);
+
+        expect(newState.data.zones?.[dzZoneCompound][0].props.id).toBe("1");
+        expect(newState.data.content[0].props.id).toBe("2");
+        expectIndexed(
+          newState,
+          newState.data.zones?.[dzZoneCompound][0],
+          [rootDroppableId, dzZoneCompound],
+          0
+        );
+        expectIndexed(newState, newState.data.content[0], [rootDroppableId], 0);
+      });
     });
 
-    it("should move from a zone to rootDroppableId", () => {
-      const state: PrivateAppState = {
-        ui: defaultUi,
-        data: {
-          ...defaultData,
-          content: [{ type: "Comp", props: { id: "1" } }],
-          zones: { zone1: [{ type: "Comp", props: { id: "2" } }] },
-        },
-        indexes: { nodes: {}, zones: {} },
-      };
-      const action: MoveAction = {
-        type: "move",
-        sourceIndex: 0,
-        sourceZone: "zone1",
-        destinationIndex: 1,
-        destinationZone: rootDroppableId,
-      };
+    describe("with slots", () => {
+      it("should reorder within a slot", () => {
+        const newState = executeSequence(defaultState, [
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: "root:slot",
+            destinationIndex: 0,
+            id: "1",
+          }),
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: "root:slot",
+            destinationIndex: 1,
+            id: "2",
+          }),
+          () => ({
+            type: "move",
+            sourceIndex: 0,
+            sourceZone: "root:slot",
+            destinationIndex: 1,
+            destinationZone: "root:slot",
+          }),
+        ]);
 
-      const newState = reducer(state, action);
-      expect(newState.data.zones?.zone1).toHaveLength(0);
-      expect(newState.data.content[1].props.id).toBe("2");
-    });
+        expect(newState.data.root.props?.slot[0].props.id).toBe("2");
+        expect(newState.data.root.props?.slot[1].props.id).toBe("1");
+        expectIndexed(
+          newState,
+          newState.data.root.props?.slot[0],
+          ["root:slot"],
+          0
+        );
+        expectIndexed(
+          newState,
+          newState.data.root.props?.slot[1],
+          ["root:slot"],
+          1
+        );
+      });
 
-    it("should move between two zones", () => {
-      const state: PrivateAppState = {
-        ui: defaultUi,
-        data: {
-          ...defaultData,
-          content: [],
-          zones: {
-            zone1: [{ type: "Comp", props: { id: "1" } }],
-            zone2: [{ type: "Comp", props: { id: "2" } }],
-          },
-        },
-        indexes: { nodes: {}, zones: {} },
-      };
-      const action: MoveAction = {
-        type: "move",
-        sourceIndex: 0,
-        sourceZone: "zone1",
-        destinationIndex: 0, // Go to the start, this time
-        destinationZone: "zone2",
-      };
+      it("should move items from a deep slot to a zone", () => {
+        const newState = executeSequence(defaultState, [
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: "root:slot",
+            destinationIndex: 0,
+            id: "1",
+          }),
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: "1:slot",
+            destinationIndex: 0,
+            id: "2",
+          }),
+          () => ({
+            type: "insert",
+            componentType: "Comp",
+            destinationZone: "2:slot",
+            destinationIndex: 0,
+            id: "3",
+          }),
+          () => ({
+            type: "move",
+            sourceIndex: 0,
+            sourceZone: "1:slot",
+            destinationIndex: 1,
+            destinationZone: dzZoneCompound,
+          }),
+        ]);
 
-      const newState = reducer(state, action);
-      expect(newState.data.zones?.zone1).toHaveLength(0);
-      expect(newState.data.zones?.zone2[0].props.id).toBe("1");
+        const item1 = newState.data.root.props?.slot[0];
+        const item2 = newState.data.zones?.[dzZoneCompound][0];
+        const item3 = newState.data.zones?.[dzZoneCompound][0].props?.slot[0];
+
+        expect(item1?.props.id).toBe("1");
+        expectIndexed(newState, item1, ["root:slot"], 0);
+
+        expect(item2?.props?.id).toBe("2");
+        expectIndexed(newState, item2, [rootDroppableId, dzZoneCompound], 0);
+
+        expect(item3?.props.id).toBe("3");
+        expectIndexed(
+          newState,
+          item3,
+          [rootDroppableId, dzZoneCompound, "2:slot"],
+          0
+        );
+      });
     });
   });
 
