798fada5a71979c9bf1e8f0fc214b1d2475028ba
Chris Villa <chrisvilla@me.com>
2025-05-02 16:04:31 +0100

    refactor: use better types for replace lib

--- packages/core/lib/data/replace.ts
+++ packages/core/lib/data/replace.ts
@@ -1,4 +1,4 @@
-export const replace = (list: any[], index: number, newItem: any) => {
+export const replace = <T>(list: T[], index: number, newItem: T) => {
   const result = Array.from(list);
   result.splice(index, 1);
   result.splice(index, 0, newItem);
