414a71baaec79244fba611bd1ff8234648e5b71a
Chris Villa <chrisvilla@me.com>
2025-05-07 15:15:10 +0100

    docs: remove premium from CTA

--- apps/docs/components/CtaCard/index.tsx
+++ apps/docs/components/CtaCard/index.tsx
@@ -10,9 +10,7 @@ export const CtaCard = () => (
     <h2 className={getClassName("heading")} id="support">
       Stuck with Puck?
     </h2>
-    <p>
-      We provide premium Puck support, design system builds, and consultancy.
-    </p>
+    <p>We provide Puck support, design system builds, and consultancy.</p>
     <div className={getClassName("actions")}>
       <DiscoveryButton />
       <Button href="https://discord.gg/D9e4E3MQVZ" variant="secondary" newTab>
