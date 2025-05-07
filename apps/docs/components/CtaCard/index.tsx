ab523dfac3c36d7e057dca612a0bee49cc99a83a
Chris Villa <chrisvilla@me.com>
2025-05-07 13:09:30 +0100

    docs: remove branding from homepage

--- apps/docs/components/CtaCard/index.tsx
+++ apps/docs/components/CtaCard/index.tsx
@@ -3,22 +3,15 @@ import getClassNameFactory from "@/core/lib/get-class-name-factory";
 import { Button } from "@/core/components/Button";
 import { DiscoveryButton } from "../DiscoveryButton";
 
-import Link from "next/link";
+const getClassName = getClassNameFactory("CtaCard", styles);
 
-const getClassName = getClassNameFactory("MeasuredCard", styles);
-
-export const MeasuredCard = () => (
+export const CtaCard = () => (
   <div className={getClassName()}>
     <h2 className={getClassName("heading")} id="support">
       Stuck with Puck?
     </h2>
     <p>
-      Puck is built by{" "}
-      <Link href="https://measured.co" target="_blank">
-        Measured
-      </Link>
-      , experts in UI strategy. We provide premium Puck support, design system
-      builds, and consultancy.
+      We provide premium Puck support, design system builds, and consultancy.
     </p>
     <div className={getClassName("actions")}>
       <DiscoveryButton />
