069bd1a66406b1a2e806d007840aaf2b57137edc
Chris Villa <chrisvilla@me.com>
2025-05-11 16:39:05 +0100

    chore: expose walkTree in rsc package

--- packages/core/rsc.tsx
+++ packages/core/rsc.tsx
@@ -2,3 +2,4 @@ export { Render } from "./components/ServerRender";
 
 export * from "./lib/resolve-all-data";
 export * from "./lib/transform-props";
+export { walkTree } from "./lib/data/walk-tree";
