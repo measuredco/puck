ef1bcf358d205ce72c2450767249209ae433b2cf
Chris Villa <chrisvilla@me.com>
2025-05-23 16:33:20 +0100

    fix: export migrate util in RSC bundle

--- packages/core/rsc.tsx
+++ packages/core/rsc.tsx
@@ -2,4 +2,5 @@ export { Render } from "./components/ServerRender";
 
 export * from "./lib/resolve-all-data";
 export * from "./lib/transform-props";
+export * from "./lib/migrate";
 export { walkTree } from "./lib/data/walk-tree";
