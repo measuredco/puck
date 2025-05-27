44110c439ef2ac3631eb0c359a88d6c70daf02f9
Chris Villa <chrisvilla@me.com>
2025-05-27 17:08:40 +0100

    docs: wire in object slot into Hero component

--- apps/demo/config/initial-data.ts
+++ apps/demo/config/initial-data.ts
@@ -20,6 +20,7 @@ export const initialData: Record<string, UserData> = {
           image: {
             url: "https://images.unsplash.com/photo-1687204209659-3bded6aecd79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
             mode: "inline",
+            content: [],
           },
           padding: "128px",
           align: "left",
